b8cc6e85e640c8ad78d541193dfadad1a2df1eab
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-04-23 16:10:39 +0200

    Change the way we store the trees in memory to always have unique names

--- src/memory.ml
+++ src/memory.ml
@@ -19,11 +19,14 @@ module Types = struct
 
   type blob = B of string
 
-  type key = string
+  type key = K of string
 
-  type label = string
+  type label = L of string
 
-  type tree = (label, key) Lib.Trie.t
+  type tree = {
+    value   : key option;
+    children: (label * key) list;
+  }
 
   type revision = {
     parents: revision list;
@@ -35,18 +38,20 @@ module Types = struct
     | Tree of tree
     | Revision of revision
 
-  type tag = string
+  type tag = T of string
 
   type t = {
     store: (key, value) Hashtbl.t;
     tags : (tag, key  ) Hashtbl.t;
   }
 
+  type remote = Uri.t
+
 end
 
 let sha1 (value: Types.value) =
   let str = Marshal.to_string value [] in
-  Lib.Misc.sha1 str
+  Types.K (Lib.Misc.sha1 str)
 
 module Low: Database.LOW with module T = Types = struct
 
@@ -77,22 +82,56 @@ module Tree: Database.TREE with module T = Types = struct
   module T = Types
   open T
 
+  type trie = (label, key) Lib.Trie.t
+
+  (* Convert a tree into a lazy trie *)
+  let rec mktrie t tree: trie =
+    let child (label, key) =
+      match Low.read t key with
+      | Some (Tree tree) -> (label, mktrie t tree)
+      | _                -> failwith "mktree" in
+    let children = lazy (
+      List.map child tree.children
+    ) in
+    Lib.Trie.create ?value:tree.value ~children ()
+
+  (* Save a lazy trie into a the database *)
+  (* XXX: not very ineficient *)
+  let rec save t (trie:trie) =
+    let children = List.map (fun (label, tree) ->
+        (label, save t tree)
+      ) (Lib.Trie.children trie) in
+    let value =
+      try Some (Lib.Trie.find trie [])
+      with Not_found -> None in
+    Low.write t (Tree { children; value })
+
+  (* Save a trie in the database and return its corresponding tree.*)
+  (* XXX: not very efficient *)
+  let mktree t trie =
+    let key = save t trie in
+    match Low.read t key with
+    | Some (Tree t) -> t
+    | _             -> failwith "tree"
+
   let get t tree labels =
-    try Some (Lib.Trie.find tree labels)
+    let trie = mktrie t tree in
+    try Some (Lib.Trie.find trie labels)
     with Not_found -> None
 
-  let callback t tree =
-    let _key = Low.write t (Types.Tree tree) in
-    ()
-
+  (* XXX: not very efficient *)
   let set t tree labels value =
+    let trie = mktrie t tree in
     let key = Low.write t value in
-    Lib.Trie.set (callback t) tree labels key
+    let trie = Lib.Trie.set trie labels key in
+    mktree t trie
 
   exception EAGAIN
   exception Invalid_values
 
   let merge t fn t1 t2 =
+    let t1 = mktrie t t1 in
+    let t2 = mktrie t t2 in
     let values l1 l2 =
       match l1, l2 with
       | [k1], [k2] ->
@@ -105,11 +144,13 @@ module Tree: Database.TREE with module T = Types = struct
         end
       | _ -> raise Invalid_values
     in
-    try Some (Lib.Trie.merge (callback t) ~values t1 t2)
+    try Some (mktree t (Lib.Trie.merge ~values t1 t2))
     with EAGAIN | Not_found -> None
 
   let succ t tree =
-    Lib.Trie.children tree
+    let trie = mktrie t tree in
+    let children = Lib.Trie.children trie in
+    List.map (fun (label,trie) -> (label, mktree t trie)) children
 
 end
 
