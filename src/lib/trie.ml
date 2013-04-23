60d3a8b4a52c6644cc1ba33edbe17b2028483be7
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-04-23 16:08:05 +0200

    Revert trie.ml to OCamlPro/ocp-index@b11cb59e02351732ac75a4929462bbcb14c947cb

--- src/lib/trie.ml
+++ src/lib/trie.ml
@@ -14,46 +14,38 @@
 
 let (!!) = Lazy.force
 
-type ('a,'b) t = {
+type ('a,'b) t =  {
   value: 'b list;
   children: ('a * ('a,'b) t) list Lazy.t;
 }
 
-type ('a, 'b) callback = ('a, 'b) t -> unit
-
-let mk callback children value =
-  let tree = { children; value } in
-  callback tree;
-  tree
-
-let children t =
-  !!(t.children)
+let children t = !!(t.children)
 
 let create ?(children = lazy []) ?value () =
   let value = match value with Some v -> [v] | None -> [] in
-  mk (fun _ -> ()) children value
+  { children; value; }
 
 let empty = create ()
 
 let rec list_map_filter f = function
   | [] -> []
   | h :: tl -> match f h with
-    | Some h -> h :: list_map_filter f tl
-    | None -> list_map_filter f tl
+      | Some h -> h :: list_map_filter f tl
+      | None -> list_map_filter f tl
 
-(* actually a map_filter, which causes it to force all the lazies
-   (it's otherwise impossible to know which branches to prune) *)
-let map_filter_values callback f tree =
-  let rec aux value children =
-    let value = list_map_filter f value in
-    let children = lazy (
+(* actually a map_filter, which causes it to force all the lazies (it's
+   otherwise impossible to know which branches to prune) *)
+let map_filter_values f tree =
+  let rec aux value children = {
+    value = list_map_filter f value;
+    children = lazy (
       list_map_filter
-        (fun (key, { value; children }) -> match aux value children with
-           | { value = []; children = lazy [] } -> None
-           | r -> Some (key, r))
+        (fun (key, {value; children}) -> match aux value children with
+          | { value = []; children = lazy [] } -> None
+          | r -> Some (key, r))
         !!children
-    ) in
-    mk callback children value
+    )
+  }
   in
   aux tree.value tree.children
 
@@ -79,12 +71,12 @@ let fold0 f tree acc =
 let fold f =
   fold0
     (fun acc path values ->
-       List.fold_left (fun acc v -> f acc path v) acc values)
+      List.fold_left (fun acc v -> f acc path v) acc values)
 
 let sub tree path =
   let rec aux tree = function
-    | [] -> tree
-    | h :: tl -> aux (List.assoc h !!(tree.children)) tl
+  | [] -> tree
+  | h :: tl -> aux (List.assoc h !!(tree.children)) tl
   in
   try aux tree path with Not_found -> empty
 
@@ -117,47 +109,47 @@ let list_map_assoc f children key empty =
   in
   aux [] children
 
-let rec map_subtree callback tree path f = match path with
+let rec map_subtree tree path f = match path with
   | [] -> f tree
   | h :: tl ->
       let children = lazy (
-        list_map_assoc (fun n ->map_subtree callback n tl f) !!(tree.children) h empty
+        list_map_assoc (fun n -> map_subtree n tl f) !!(tree.children) h empty
       ) in
-      mk callback children tree.value
+      { tree with children }
 
-let set callback tree path value =
-  map_subtree callback tree path (fun t -> mk callback t.children [value])
+let set tree path value =
+  map_subtree tree path (fun t -> { t with value = [value] })
 
-let set_lazy callback tree path lazy_value =
-  map_subtree callback tree path (fun t -> mk callback t.children [!!lazy_value])
+let set_lazy tree path lazy_value =
+  map_subtree tree path (fun t -> { t with value = [!!lazy_value] })
 
-let add callback tree path value =
-  map_subtree callback tree path (fun t -> mk callback t.children (value::t.value))
+let add tree path value =
+  map_subtree tree path (fun t -> { t with value = value::t.value })
 
-let unset callback tree path =
-  map_subtree callback tree path (fun t -> mk callback t.children [])
+let unset tree path =
+  map_subtree tree path (fun t -> { t with value = [] })
 
-let rec filter_keys callback f tree =
-  let children = lazy (
-    list_map_filter
-      (fun (key,n) -> if f key then Some (key, filter_keys callback f n) else None)
-      !!(tree.children)
-  ) in
-  mk callback children tree.value
+let rec filter_keys f tree =
+  { tree with
+    children = lazy (
+      list_map_filter
+        (fun (key,n) -> if f key then Some (key, filter_keys f n) else None)
+        !!(tree.children)
+    )}
 
-let graft callback tree path node =
-  map_subtree callback tree path (fun t -> mk callback node.children t.value)
+let graft tree path node =
+  map_subtree tree path (fun t -> { t with children = node.children })
 
-let graft_lazy callback tree path lazy_node =
-  map_subtree callback tree path
-    (fun t -> mk callback (lazy !!(!!lazy_node.children)) t.value)
+let graft_lazy tree path lazy_node =
+  map_subtree tree path
+    (fun t -> { t with children = lazy !!(!!lazy_node.children) })
 
-let rec merge callback ?(values = fun v1 v2 -> v2@v1) t1 t2 =
+let rec merge ?(values = fun v1 v2 -> v2@v1) t1 t2 =
   let rec aux l1 l2 = match l1,l2 with
     | ((k1,v1) as h1 :: tl1), ((k2,v2) as h2 :: tl2) ->
         if k1 < k2 then h1 :: aux tl1 l2 else
         if k2 < k1 then h2 :: aux l1 tl2 else
-          (k1, merge callback ~values v1 v2) :: aux tl1 tl2
+          (k1, merge ~values v1 v2) :: aux tl1 tl2
     | [], l | l, [] -> l
   in
   let value = values t1.value t2.value in
@@ -167,7 +159,7 @@ let rec merge callback ?(values = fun v1 v2 -> v2@v1) t1 t2 =
     let c2 = List.sort compare_keys (Lazy.force t2.children) in
     aux c1 c2
   ) in
-  mk callback children value
+  {value; children}
 
-let append callback tree (path,node) =
-  map_subtree callback tree path (fun t -> merge callback t node)
+let append tree (path,node) =
+  map_subtree tree path (fun t -> merge t node)
