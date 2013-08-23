e54fc51c99eeecb59374c4b8cb0aa2bae432fb56
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-08-23 13:05:16 +0200

    Add a (failing) test for key serialization

--- tests/test_disk.ml
+++ tests/test_disk.ml
@@ -1,25 +1,19 @@
 open IrminLwt
 open OUnit
 
-(* values *)
-let v1 = Value.blob "foo"
-let v2 = Value.blob "bar"
-
 let cmp_opt fn x y =
   match x, y with
   | Some x, Some y -> fn x y
   | _ -> false
 
-let test_db = "test-db"
-
-let clean () =
+let clean test_db =
   if Sys.file_exists test_db then
     let cmd = Printf.sprintf "rm -rf %s" test_db in
     let _ = Sys.command cmd in
     ()
 
-let with_db fn =
-  clean ();
+let with_db test_db fn =
+  clean test_db;
   lwt () = Disk.init test_db in
   let t = Disk.create test_db in
   try_lwt fn t
@@ -29,6 +23,9 @@ let with_db fn =
 let assert_key_equal =
   assert_equal ~cmp:Key.equal ~printer:Key.pretty
 
+let assert_keys_equal =
+  assert_equal ~cmp:Key.Set.equal ~printer:Key.Set.pretty
+
 let assert_value_opt_equal =
   let printer = function
     | None   -> "<none>"
@@ -36,6 +33,9 @@ let assert_value_opt_equal =
   assert_equal ~cmp:(cmp_opt Value.equal) ~printer
 
 let test_values () =
+  let v1 = Value.blob "foo" in
+  let v2 = Value.blob "" in
+  let test_db = "test-db" in
   let module DV = Disk.Value_store in
   let test t =
     lwt k1 = DV.write t v1 in
@@ -50,12 +50,29 @@ let test_values () =
     assert_value_opt_equal (Some v2) v2';
     Lwt.return ()
   in
-  Lwt_unix.run (with_db test)
+  Lwt_unix.run (with_db test_db test)
+
+let test_keys () =
+  let k1 = Value.key (Value.blob "foo") in
+  let k2 = Value.key (Value.blob "") in
+  let k1s = Key.Set.singleton k1 in
+  let k2s = Key.Set.singleton k2 in
+  let test_db = "test-db" in
+  let module KV = Disk.Key_store in
+  let test t =
+    lwt () = KV.add t k1 k2s in
+    lwt k1s' = KV.succ t k2 in
+    lwt k2s' = KV.pred t k1 in
+    assert_keys_equal k1s k1s';
+    assert_keys_equal k2s k2s';
+    Lwt.return ()
+  in
+  Lwt_unix.run (with_db test_db test)
 
 let suite =
   "disk" >:::
-    [" Basic disk operations for values"
-     >:: test_values
+    ["Basic disk operations for values" >:: test_values;
+     "Basic disk operations for keys"   >:: test_keys;
     ]
 
 let _ =
