419ab7b225d7348aac5497dec58a008a48fa642d
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-11-22 20:49:57 +0100

    Improve the documentation of IrminBuffer

--- src/core/irminBuffer.mli
+++ src/core/irminBuffer.mli
@@ -16,99 +16,101 @@
 
 (** Bigarrays *)
 
-type ba = Cstruct.buffer
+type t
+(** Type for mutable windows on top of bigarrays, with bounds
+    automatically updated when values are read/write in the buffer. *)
 
-(** Create a bigarray. *)
-val create_ba: int -> ba
-
-(** Dump a bigarray. *)
-val dump_ba: ?msg:string -> ba -> unit
-
-(** Pretty print a big array. *)
-val pretty_ba: ba -> string
-
-(** Length of a bigarray. *)
-val length_ba: ba -> int
-
-(** Bound-mutable bigarrays. *)
-type t = {
-  mutable buffer: Cstruct.t;
-}
-
-(** Create a new buffer. *)
 val create: int -> t
+(** Create a new buffer. *)
 
 (** Get the buffer length. *)
 val length: t -> int
 
-(** Create a new buffer. *)
-val of_ba: ba -> t
-
-(** Accessor. Return the underlying big array. *)
-val to_ba: t -> ba
-
 (** {2 Errors} *)
 
-(** Parse error *)
 exception Parse_error of string
+(** Parse error *)
 
-(** Eventualy raise a exception *)
 val parse_error_buf: t -> ('a, unit, string, 'b) format4 -> 'a
+(** Eventualy raise a exception *)
 
-(** Raise an exception *)
 val parse_error: ('a, unit, string, 'b) format4 -> 'a
+(** Raise an exception *)
 
-(** Dump the buffer *)
 val dump: ?msg:string -> t -> unit
+(** Dump the buffer *)
 
 (** {Basic IO operations} *)
 
 (** Get/set big-endian integers of various sizes. *)
 
-(** [get_char buf] return the character stored in [buf]. *)
 val get_char: t -> char
+(** [get_char buf] return the character stored in [buf]. *)
 
-(** [get_uint8 buf] is the 8 bit unsigned integer stored in [buf]. *)
 val get_uint8: t -> int
+(** [get_uint8 buf] is the 8 bit unsigned integer stored in [buf]. *)
 
+val get_uint16: t -> int
 (** [get_uint16 buf] is the 16 bit long big-endian unsigned integer
     stored in [buf]. *)
-val get_uint16: t -> int
 
+val get_uint32: t -> int32
 (** [get_uint32 buf] is the 32 bit long big-endian unsigned integer
     stored in [buf]. *)
-val get_uint32: t -> int32
 
+val get_uint64: t -> int64
 (** [get_uint64 buf] is the 64 bit long big-endian unsigned integer
     stored in [buf]. *)
-val get_uint64: t -> int64
 
-(** [get_string buf len] is the string of size [len] stored in [buf]. *)
 val get_string: t -> int -> string
+(** [get_string buf len] is the string of size [len] stored in [buf]. *)
 
 val pick_string: t -> int -> string option
 (** [pick_string buf len] looks for the string of size [len] in the
     buffer, without consuming it. Return [None] if the buffer is
     bigger than [len]. *)
 
+val set_char: t -> char -> unit
 (** [set_char buf off c] write the character [c] in [buf] at offset
     [off]. *)
-val set_char: t -> char -> unit
 
-(** [set_uint8 buf] write the 8 bit long integer stored in [buf]. *)
 val set_uint8: t -> int -> unit
+(** [set_uint8 buf] write the 8 bit long integer stored in [buf]. *)
 
+val set_uint16: t -> int -> unit
 (** [set_uint16 buf i] writes the 16 bit long big-endian unsigned
     integer [i] in [buf]. *)
-val set_uint16: t -> int -> unit
 
+val set_uint32: t -> int32 -> unit
 (** [set_uint32 buf i] writes the 32 bit long big-endian unsigned
     integer [i] in [buf]. *)
-val set_uint32: t -> int32 -> unit
 
+val set_uint64: t -> int64 -> unit
 (** [set_uint64 buf i] writes the 64 bit long big-endian unsigned
     integer [i] in [buf]. *)
-val set_uint64: t -> int64 -> unit
 
-(** [set_string buf str] write the string [str] into [buf]. *)
 val set_string: t -> string -> unit
+(** [set_string buf str] write the string [str] into [buf]. *)
+
+(** {2 Bigarrays} *)
+
+type ba = Cstruct.buffer
+(** ocaml-cstruct's buffers are windows on top of bigarrays. *)
+
+(** Create a bigarray. *)
+val create_ba: int -> ba
+
+val dump_ba: ?msg:string -> ba -> unit
+(** Dump a bigarray. *)
+
+val pretty_ba: ba -> string
+(** Pretty print a big array. *)
+
+(** Length of a bigarray. *)
+val length_ba: ba -> int
+
+(** Create a new buffer. *)
+val of_ba: ba -> t
+
+(** Accessor. Return the underlying big array. *)
+val to_ba: t -> ba
