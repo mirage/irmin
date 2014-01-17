78cd00e07cf8618c105c831fb19fdf50218c3185
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-17 02:15:40 +0000

    travis ++

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,12 +7,6 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-export OPAMVERBOSE=1
-echo OCaml version
-ocaml -version
-echo OPAM versions
-opam --version
-opam --git-version
 
 opam init
 opam install ${OPAM_PACKAGES}
