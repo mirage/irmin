ad3306399d2981e7aefa4222683fcc8550fbe559
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-17 02:11:43 +0000

    More travis fixes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,6 @@
 # OPAM packages needed to build tests.
 OPAM_PACKAGES="ezjsonm ocamlgraph lwt cryptokit re dolog mstruct core_kernel uri \
-               cohttp ssl core_kernel cmdliner"
+               cohttp ssl core_kernel cmdliner alcotest"
 
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
