c7e8900c4395533ab344e89eb01a7ca38820e140
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-17 00:32:06 +0000

    Add Travis CI scripts

---
+++ .travis-ci.sh
@@ -0,0 +1,16 @@
+# OPAM packages needed to build tests.
+OPAM_PACKAGES="ezjsonm ocamlgraph lwt cryptokit re dolog mstruct core_kernel uri \
+               cohttp ssl core_kernel cmdliner alcotest"
+
+ppa=avsm/ocaml41+opam11
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+
+opam init
+opam install ${OPAM_PACKAGES}
+
+eval `opam config env`
+make
+make test
