f291a907bbbb5e3ea5212781294c12aeefda04f6
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-04-19 12:27:11 +0100

    Add an other dispatch backend

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -23,6 +23,7 @@ let () =
     `Quick, Test_dispatch.suite k [|
       Test_git.suite k `Disk;
       Test_memory.suite k;
+      Test_fs.suite k;
     |];
     `Slow , Test_crud.suite k (Test_memory.suite k);
     `Slow , Test_crud.suite k (Test_fs.suite k);
