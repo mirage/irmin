ba7516c2cb8fc846aa9f67376303baf5797fa188
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-04-19 12:23:36 +0100

    Update deps in .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="ezjsonm ocamlgraph lwt cryptokit re dolog mstruct core_kernel uri \
+OPAM_PACKAGES="ezjsonm ocamlgraph lwt sha re dolog mstruct core_kernel uri \
                cohttp ssl core_kernel cmdliner alcotest git"
 
 ppa=avsm/ocaml41+opam11
