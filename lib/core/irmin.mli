907f20ddad5ab4e872b0c9d22386f9adc64a0dd9
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-04-21 21:36:54 +0100

    Add some comments on the semantics of Irmin.updates

--- lib/core/irmin.mli
+++ lib/core/irmin.mli
@@ -77,7 +77,9 @@ module type S = sig
   (** Load sub-trees in memory. *)
 
   val updates: t -> key -> View.t -> unit Lwt.t
-  (** Commit a view to the store. *)
+  (** Commit a view to the store. The view *replaces* the current
+      subtree, so if you want to do a merge, you have to do it manually
+      (by creating a new branch, or rebasing before commiting). *)
 
   val view: t -> key -> View.t Lwt.t
   (** Build a view from the store. *)
