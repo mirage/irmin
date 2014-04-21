885a3e9509c6b9fa8e0c672ea41da0d5a3c30ce4
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-04-21 18:46:33 +0100

    All the dispatched backends should have the same serialization format
    Eg. git only (GitMemory or GitDisk) or bin_io only (IrminFS or IrminMemory). This needs to be cleaned up later.

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -21,7 +21,7 @@ let () =
     `Quick, Test_git.suite k `Disk;
     `Quick, Test_git.suite k `Memory;
     `Quick, Test_dispatch.suite k [|
-      Test_git.suite k `Disk;
+(*      Test_git.suite k `Disk; *)
       Test_memory.suite k;
       Test_fs.suite k;
     |];
