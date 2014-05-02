685678ca37b26648009bc861bd3fde44d2672b52
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-05-02 13:17:17 +0100

    Add some documentation to IrminDispatch

--- lib/backend/irminDispatch.mli
+++ lib/backend/irminDispatch.mli
@@ -14,17 +14,38 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-module Make
+(** Dispatch between multiple backends. *)
+
+module Key
     (K: IrminKey.S)
     (C: IrminContents.S)
     (R: IrminReference.S) :
 sig
 
+  (** Dispatch based on the SHA1. All the backend should use the same
+      function to compute a key from a value, and every dispatch
+      usually involves computing the hash of the value to know which
+      backend to choose, so it might be a bit slow. *)
+
+
+  (** This kind of low-level dispatch can be used to distribute the
+      values based on their hash, typically using a DHT implementation
+      (which is not provided here). *)
+
   type value = (K.t, C.t) IrminValue.t
+  (** Type of values. *)
 
   val create: key:(K.t -> int) -> value:(value -> (K.t -> unit) * int) ->
     (K.t, C.t, R.t) Irmin.t array ->  (K.t, C.t, R.t) Irmin.t
+  (** [create ~key ~value backends] create a new store where every reads
+      and writes will be dispatched to one of the [backends]. The [key]
+      function return the offset of the backend in [backends] to read
+      from. The [value] function returns a callback function to register the
+      key of the value, and the offset of the backend in [backends] to write
+      to. *)
+
 
   val cast: (K.t, C.t, R.t) Irmin.t -> (module Irmin.S)
+  (** Cast to an abstract Irminsule store. *)
 
 end
