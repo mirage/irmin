957934b9368e93f25c5eb06a148964250aba9345
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-05-27 20:07:01 +0100

    Update travis file

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,7 @@
 # OPAM packages needed to build tests.
+# We need cryptokit to run the tests
 OPAM_PACKAGES="ezjsonm ocamlgraph lwt sha re dolog mstruct core_kernel uri \
-               cohttp ssl core_kernel cmdliner alcotest git"
+               cohttp ssl core_kernel cmdliner alcotest git cryptokit"
 
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
@@ -9,7 +10,6 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 
 opam init
-opam pin git https://github.com/samoht/ocaml-git.git
 opam install ${OPAM_PACKAGES}
 
 eval `opam config env`
