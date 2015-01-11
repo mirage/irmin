a362d0433e1793144de08f501171f1c08871d1e2
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-01-11 20:20:24 +0000

    Unbreak 4.01 build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -639,7 +639,7 @@ let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 # 640 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
-Ocamlbuild_pack.Flags.mark_tag_used "tests";;
+(* Ocamlbuild_pack.Flags.mark_tag_used "tests";; *)
 let () =
   flag ["ocaml"; "doc"] (A"-colorize-code");
   flag ["ocaml"; "doc"] (A"-short-functors");
