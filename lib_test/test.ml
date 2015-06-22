358f35d0fb12ea3d76838b8ec70d0b014e47b36d
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-06-22 13:07:33 +0200

    Test again the HTTP backend on Travis

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -19,7 +19,7 @@ let misc = [
 ]
 
 let suite k =
-  let depends = if k = `String then `Slow (* `Quick *) else `Slow in
+  let depends = if k = `String then `Quick else `Slow in
   [
     `Quick , Test_memory.suite k;
     `Quick , Test_fs.suite k;
