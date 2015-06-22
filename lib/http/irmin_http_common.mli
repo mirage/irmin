a74fd49974c2f7101b778ada24502327e619aeaa
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-06-22 12:55:40 +0200

    Fix the small race in the HTTP backend
    - Now all low-level function in the tag store can take the tag lock if necessary
    - The watch function now are a bit more synchronous: the server first sends a
      token on the stream and the client waits for that token before returning to
      the user. This let compose watch operations on the client a bit more reliably
    - move some common code in a shared module between the client and the server
    - Clean-up watches on the server when the client closes the stream.
    
    Fix #198

---
+++ lib/http/irmin_http_common.mli
@@ -0,0 +1,23 @@
+(*
+ * Copyright (c) 2013-2015 Thomas Gazagnaire <thomas@gazagnaire.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+(** HTTP helpers *)
+
+val ok_or_error: [`Ok | `Error] Tc.t
+val ok_or_duplicated_tag: [`Ok | `Duplicated_tag] Tc.t
+val lca: 'a Tc.t -> [`Ok of 'a list | `Max_depth_reached | `Too_many_lcas] Tc.t
+
+val start_stream: string
