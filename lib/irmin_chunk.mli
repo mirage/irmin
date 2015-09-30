6818c07da7582279f6306df29193f8e9b0860a43
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-09-30 23:23:49 +0100

    More doc tweaks

--- lib/irmin_chunk.mli
+++ lib/irmin_chunk.mli
@@ -18,16 +18,16 @@
 (** Managing Chunks.
 
     This module exposes functors to store raw contents into
-    append-only stores as chunks of same size. It exposes the {!AO}
-    functor which split the raw contents into [Data] blocks, addressed
-    by [Node] blocks. That's the usual rope-like representation of
-    strings, but chunk trees are always build as perfectly
-    well-balanced and blocks are addressed by their hash (or by the
-    stable keys returned by the underlying store).
+    append-only stores as chunks of same size. It exposes the
+    {{!AO}AO} functor which split the raw contents into [Data] blocks,
+    addressed by [Node] blocks. That's the usual rope-like
+    representation of strings, but chunk trees are always build as
+    perfectly well-balanced and blocks are addressed by their hash (or
+    by the stable keys returned by the underlying store).
 
     A chunk has the following structure:
 
-{v
+    {v
      --------------------------
      | uint8_t type            |
      ---------------------------
@@ -41,10 +41,11 @@ v}
     data, [length] is the payload length. Otherwise it is the number
     of children that the node has.
 
-    It also exposes {!AO_stable} which -- as {!AO} does -- stores raw
-    contents into chunks of same size. But it also preserves the nice
-    properpty that values are addressed by their hash. instead of by
-    the hash of the root chunk node as it is the case for {!AO}.
+    It also exposes {{!AO_stable}AO_stable} which -- as {{!AO}AO} does
+    -- stores raw contents into chunks of same size. But it also
+    preserves the nice properpty that values are addressed by their
+    hash. instead of by the hash of the root chunk node as it is the
+    case for {{!AO}AO}.
 *)
 
 val chunk_size: int Irmin.Private.Conf.key
@@ -56,7 +57,7 @@ val config:
   ?config:Irmin.config -> ?size:int -> ?min_size:int -> unit -> Irmin.config
 (** [config ?config ?size ?min_size ()] is the configuration value
     extending the optional [config] with bindings associating
-    {!chunk_size} to [size].
+    {{!chunk_size}chunk_size} to [size].
 
     Fail with [Invalid_argument] if [size] is smaller than [min_size].
     [min_size] is, by default, set to 4000 (to avoid hash colision on
@@ -66,14 +67,16 @@ val config:
 
 module AO (S: Irmin.AO_MAKER_RAW): Irmin.AO_MAKER_RAW
 (** [AO(X)] is an append-only store which store values cut into chunks
-    into the underlying store [X]. The keys returns by [add] are the
-    hash of the chunked values: it could either be a full block if the
-    value is small, or a tree node if the values need to be cut into
-    chunks.
+    into the underlying store [X].
+
+    The keys returns by [add] are the hash of the chunked values: it could
+    either be a full block if the value is small, or a tree node if
+    the values need to be cut into chunks.
 
     In both case, the return hash will be different from the hash of
-    the value. This discrepency can be fixed using the an
-    {{!Irmin.LINK}immutable link store}. *)
+    the value. This discrepency can be fixed using
+    {{!AO_stable}AO_stable}, at the cost of adding an indirection on
+    reads. *)
 
 module AO_stable (L: Irmin.LINK_MAKER) (S: Irmin.AO_MAKER_RAW):
   Irmin.AO_MAKER_RAW
