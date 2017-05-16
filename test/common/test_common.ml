88787a8be8f42daee5ac927de173ba04d31b5a08
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-05-16 12:09:58 -0700

    Use mtime 1.0

--- test/common/test_common.ml
+++ test/common/test_common.ml
@@ -33,7 +33,7 @@ let reporter ?(prefix="") () =
     let k _ = over (); k () in
     let ppf = match level with Logs.App -> Fmt.stdout | _ -> Fmt.stderr in
     let with_stamp h _tags k fmt =
-      let dt = Mtime.to_us (Mtime.elapsed ()) in
+      let dt = Mtime.Span.to_us (Mtime_clock.elapsed ()) in
       Fmt.kpf k ppf ("%s%0+04.0fus %a %a @[" ^^ fmt ^^ "@]@.")
         prefix
         dt
