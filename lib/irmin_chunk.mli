b9bf2684a95bf96e6c8671db406ab98ccd1e7604
Gabriel <g2p.code@gmail.com>
2017-07-20 13:34:33 +0200

    Preserve the README as documentation comments

--- lib/irmin_chunk.mli
+++ lib/irmin_chunk.mli
@@ -15,6 +15,29 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
+(**
+    This package provides an Irmin backend to cut raw contents into blocks
+    of the same size, while preserving the keys used in the store. It can
+    be used to optimize space usage when dealing with large files or as a
+    an intermediate layer for a raw block device backend.
+
+    # Install
+
+    Use opam:
+
+    ```shell
+    opam install irmin-chunk
+    ```
+
+    # Use
+
+    ```ocaml
+    (* Build an Irmin store, where blobs are cut into chunks of same size *)
+    module AO = Irmin_chunk.AO_stable(Irmin_mem.Link)(Irmin_mem.AO)
+    module Store = Irmin.Make(AO)(Irmin_mem.RW)
+    ```
+*)
+
 (** Managing Chunks.
 
     This module exposes functors to store raw contents into
