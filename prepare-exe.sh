57ca420960c683a41f34c1ada8c4ab0830f3ce8b
Anil Madhavapeddy <anil@recoil.org>
2019-01-19 10:42:39 +0000

    remove old ocamlbuild-based prepare-exe.sh

--- prepare-exe.sh
+++
@@ -1,6 +0,0 @@
-OUTPUT=output
-mkdir -p $OUTPUT
-cp _build/lib_test/test.native $OUTPUT/irmin-test.exe
-cp _build/bin/main.native $OUTPUT/irmin.exe
-cp "/usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgmp-10.dll" $OUTPUT
-cp "/usr/x86_64-w64-mingw32/sys-root/mingw/bin/zlib1.dll" $OUTPUT
