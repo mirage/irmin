ad937bbdc58a461a50f88b989759894c41ae983e
Andreas Garnaes <andreas.garnaes@gmail.com>
2019-02-15 09:44:51 +0100

    Add diff subscription to irmin-graphql

--- src/irmin-graphql/irmin_graphql.mli
+++ src/irmin-graphql/irmin_graphql.mli
@@ -1,14 +1,22 @@
 module Schema : Graphql_intf.Schema with type 'a Io.t = 'a Lwt.t
 
 module type S = sig
+  module IO : Cohttp_lwt.S.IO
   type store
   type server
+
+  type response_action =
+    [ `Expert of Cohttp.Response.t
+                 * (IO.ic
+                    -> IO.oc
+                    -> unit Lwt.t)
+    | `Response of Cohttp.Response.t * Cohttp_lwt.Body.t ]
 
   val schema : store -> unit Schema.schema
   val execute_request :
       unit Schema.schema ->
       Cohttp_lwt.Request.t ->
-      Cohttp_lwt.Body.t -> (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t
+      Cohttp_lwt.Body.t -> response_action Lwt.t
   val server : store -> server
 end
 
