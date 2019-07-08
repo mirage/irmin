6e147bc7dedd07d7ff76cb9ad1d380bd905011a4
Craig Ferguson <cf443@cam.ac.uk>
2019-07-08 15:46:00 +0200

    feat: refactor into Located functor

--- deriver/ppx_irmin.ml
+++ deriver/ppx_irmin.ml
@@ -2,154 +2,153 @@ open Ppxlib
 
 let name = "irmin"
 
-let rec derive_core: location -> core_type -> expression = fun loc typ ->
-  let (module S) = Ast_builder.make loc in
-  let open S in
+module type S = sig
+  val derive_core: core_type -> expression
+  val derive_variant: string -> constructor_declaration list -> expression
+end
 
-  match typ.ptyp_desc with
-  | Ptyp_constr ({txt = Lident const_name; _}, args) ->
-    List.map (fun t -> (Nolabel, derive_core loc t)) args
-    |> pexp_apply (pexp_ident @@ Located.lident const_name)
+module Located (S: Ast_builder.S): S = struct
+  open S
 
-  | Ptyp_tuple args -> derive_tuple loc args
+  let rec derive_core: core_type -> expression = fun typ ->
+    match typ.ptyp_desc with
+    | Ptyp_constr ({txt = Lident const_name; _}, args) ->
+      List.map (fun t -> (Nolabel, derive_core t)) args
+      |> pexp_apply (pexp_ident @@ Located.lident const_name)
 
-  | Ptyp_arrow _ -> invalid_arg "Arrow types unsupported"
-  | _ -> invalid_arg "Unsupported type"
+    | Ptyp_tuple args -> derive_tuple args
 
-
-and derive_tuple: location -> core_type list -> expression  = fun loc args ->
-  let (module S) = Ast_builder.make loc in
-  let open S in
-
-  let tuple_type = match List.length args with
-    | 2 -> "pair"
-    | 3 -> "triple"
-    | l -> invalid_arg (Printf.sprintf "Tuples must have 2 or 3 components. Found %d." l)
-  in
-  List.map (fun t -> (Nolabel, derive_core loc t)) args
-  |> pexp_apply (pexp_ident @@ Located.lident tuple_type)
+    | Ptyp_arrow _ -> invalid_arg "Arrow types unsupported"
+    | _ -> invalid_arg "Unsupported type"
 
 
-and derive_variant: location -> string -> constructor_declaration list -> expression = fun loc name cs ->
-  let (module S) = Ast_builder.make loc in
-  let open S in
+  and derive_tuple: core_type list -> expression = fun args ->
+    let tuple_type = match List.length args with
+      | 2 -> "pair"
+      | 3 -> "triple"
+      | l -> invalid_arg (Printf.sprintf "Tuples must have 2 or 3 components. Found %d." l)
+    in
+    List.map (fun t -> (Nolabel, derive_core t)) args
+    |> pexp_apply (pexp_ident @@ Located.lident tuple_type)
 
 
-  let fparam_of_cdecl c =
-    c.pcd_name.txt
-    |> String.lowercase_ascii
-  in
+  and derive_variant: string -> constructor_declaration list -> expression = fun name cs ->
+    let fparam_of_cdecl c =
+      c.pcd_name.txt
+      |> String.lowercase_ascii
+    in
 
-  let function_of_cdecl c =
-    fparam_of_cdecl c
-    |> Located.mk
-    |> ppat_var
-    |> pexp_fun Nolabel None
-  in
+    let function_of_cdecl c =
+      fparam_of_cdecl c
+      |> Located.mk
+      |> ppat_var
+      |> pexp_fun Nolabel None
+    in
 
-  (* | Cons_name (x1, x2, x3) -> cons_name x1 x2 x3 *)
-  let pcase_of_cdecl c =
-    match c.pcd_args with
-    | Pcstr_tuple [] ->
-      case
-        ~lhs:(ppat_construct (Located.map_lident c.pcd_name) None)
-        ~guard:None
-        ~rhs:(pexp_ident @@ Located.lident (fparam_of_cdecl c))
+    let generate_identifiers =
+      List.mapi (fun i _ -> Printf.sprintf "x%d" (i+1))
+    in
 
-    | Pcstr_tuple components ->
-      let idents =
-        (* TODO: refactor out magic identifier 'x' ? *)
-        List.mapi (fun i _ -> Printf.sprintf "x%d" (i+1)) components
-      in
-      case
-        ~lhs:(ppat_construct
-                (Located.map_lident c.pcd_name)
-                (idents
-                 |> List.map Located.mk
-                 |> List.map ppat_var
-                 |> ppat_tuple
-                 |> fun x -> Some x))
-        ~guard:None
-        ~rhs:(pexp_apply
-                (pexp_ident @@ Located.lident (fparam_of_cdecl c))
-                [(Nolabel, (pexp_tuple
-                (idents
-                 |> List.map Located.lident
-                 |> List.map pexp_ident)))]
-             )
+    (* | Cons_name (x1, x2, x3) -> cons_name x1 x2 x3 *)
+    let pcase_of_cdecl c =
+      match c.pcd_args with
+      | Pcstr_tuple [] ->
+        case
+          ~lhs:(ppat_construct (Located.map_lident c.pcd_name) None)
+          ~guard:None
+          ~rhs:(pexp_ident @@ Located.lident (fparam_of_cdecl c))
 
-    | Pcstr_record _ -> invalid_arg "Record types unsupported"
-  in
+      | Pcstr_tuple components ->
+        let idents = generate_identifiers components in
+        case
+          ~lhs:(ppat_construct
+                  (Located.map_lident c.pcd_name)
+                  (idents
+                   |> List.map Located.mk
+                   |> List.map ppat_var
+                   |> ppat_tuple
+                   |> fun x -> Some x))
+          ~guard:None
+          ~rhs:(pexp_apply
+                  (pexp_ident @@ Located.lident (fparam_of_cdecl c))
+                  [(Nolabel, (pexp_tuple
+                                (idents
+                                 |> List.map Located.lident
+                                 |> List.map pexp_ident)))])
 
-  let vcase_of_cdecl c = fun e ->
-    let cons_name = c.pcd_name.txt in
-    match c.pcd_args with
-    | Pcstr_record _ -> invalid_arg "Record types unsupported"
-    | Pcstr_tuple [] -> (
-        (* |~ case0 "cons_name" Cons_name *)
-        pexp_apply (pexp_ident @@ Located.lident "|~") [
-          (Nolabel, e);
-          (Nolabel, pexp_apply (pexp_ident @@ Located.lident "case0") [
-              (Nolabel, pexp_constant @@ Pconst_string (cons_name, None));
-              (Nolabel, pexp_construct (Located.lident cons_name) None)
-            ])
-        ])
-    | Pcstr_tuple components -> (
-        (* |~ case1 "cons_name" component_type (fun (x1, ..., xN) -> Cons_name (x1, ..., xN)) *)
+      | Pcstr_record _ -> invalid_arg "Record types unsupported"
+    in
 
-        let idents =
-          (* TODO: refactor out magic identifier 'x' ? *)
-          List.mapi (fun i _ -> Printf.sprintf "x%d" (i+1)) components
-        in
+    let vcase_of_cdecl c = fun e ->
+      let cons_name = c.pcd_name.txt in
+      match c.pcd_args with
+      | Pcstr_record _ -> invalid_arg "Record types unsupported"
+      | Pcstr_tuple [] -> (
+          (* |~ case0 "cons_name" Cons_name *)
+          pexp_apply (pexp_ident @@ Located.lident "|~") [
+            (Nolabel, e);
+            (Nolabel, pexp_apply (pexp_ident @@ Located.lident "case0") [
+                (Nolabel, pexp_constant @@ Pconst_string (cons_name, None));
+                (Nolabel, pexp_construct (Located.lident cons_name) None)
+              ])
+          ])
+      | Pcstr_tuple components -> (
+          (* |~ case1 "cons_name" component_type (fun (x1, ..., xN) -> Cons_name (x1, ..., xN)) *)
 
-        pexp_apply (pexp_ident @@ Located.lident "|~") [
-          (Nolabel, e);
-          (Nolabel, pexp_apply (pexp_ident @@ Located.lident "case1") [
-              (Nolabel, pexp_constant @@ Pconst_string (cons_name, None));
-              (Nolabel,
-               match components with
-               | [t] -> derive_core loc t
-               | c   -> derive_tuple loc c);
-              (Nolabel, pexp_fun
-                 Nolabel
-                 None
-                 (idents
-                  |> List.map Located.mk
-                  |> List.map ppat_var
-                  |> ppat_tuple)
-                 (pexp_construct
-                    (Located.lident cons_name)
-                    (Some (pexp_tuple
-                              (idents
-                               |> List.map Located.lident
-                               |> List.map pexp_ident))))
-              )
-            ])
-        ])
-  in
+          let idents = generate_identifiers components in
 
-  let compose_all = List.fold_left (fun f g x -> f (g x)) (fun x -> x) in
+          pexp_apply (pexp_ident @@ Located.lident "|~") [
+            (Nolabel, e);
+            (Nolabel, pexp_apply (pexp_ident @@ Located.lident "case1") [
+                (Nolabel, pexp_constant @@ Pconst_string (cons_name, None));
+                (Nolabel,
+                 match components with
+                 | [t] -> derive_core t
+                 | c   -> derive_tuple c);
+                (Nolabel, pexp_fun
+                   Nolabel
+                   None
+                   (idents
+                    |> List.map Located.mk
+                    |> List.map ppat_var
+                    |> ppat_tuple)
+                   (pexp_construct
+                      (Located.lident cons_name)
+                      (Some (pexp_tuple
+                               (idents
+                                |> List.map Located.lident
+                                |> List.map pexp_ident))))
+                )
+              ])
+          ])
+    in
 
-  let nested = List.map function_of_cdecl cs |> compose_all in
-  let cases = List.rev cs |> List.map vcase_of_cdecl |> compose_all in
+    let compose_all = List.fold_left (fun f g x -> f (g x)) (fun x -> x) in
 
-  let pattern =
-    List.map pcase_of_cdecl cs
-    |> pexp_function
-  in
+    let nested = List.map function_of_cdecl cs |> compose_all in
+    let cases = List.rev cs |> List.map vcase_of_cdecl |> compose_all in
 
-  pexp_apply (pexp_ident @@ Located.lident "|>")
-    [
-      (Nolabel, cases @@ pexp_apply (pexp_ident @@ Located.lident "variant") [
-          (Nolabel, estring name);
-          (Nolabel, nested pattern)
-        ]);
-      (Nolabel, pexp_ident @@ Located.lident "sealv")
-    ]
+    let pattern =
+      List.map pcase_of_cdecl cs
+      |> pexp_function
+    in
+
+    (* If the type is recursive, wrap everything in a mu (fun cons_name -> ... )*)
+    pexp_apply (pexp_ident @@ Located.lident "|>")
+      [
+        (Nolabel, cases @@ pexp_apply (pexp_ident @@ Located.lident "variant") [
+            (Nolabel, estring name);
+            (Nolabel, nested pattern)
+          ]);
+        (Nolabel, pexp_ident @@ Located.lident "sealv")
+      ]
+end
 
 let expand_str ~loc ~path:_ (input_ast: rec_flag * type_declaration list) =
   let (module S) = Ast_builder.make loc in
+  let (module L) = (module Located(S): S) in
   let open S in
+
 
   let open_type = pexp_open Fresh (Located.lident "Irmin.Type") in
 
@@ -171,10 +170,10 @@ let expand_str ~loc ~path:_ (input_ast: rec_flag * type_declaration list) =
                     evar ("Irmin.Type." ^ type_name)
 
                   (* Type constructor: list, tuple,  etc. *)
-                  | _ -> derive_core loc c |> open_type
+                  | _ -> L.derive_core c |> open_type
                 ))
 
-          | Ptype_variant cs -> derive_variant loc name cs |> open_type
+          | Ptype_variant cs -> L.derive_variant name cs |> open_type
           | Ptype_record _ -> invalid_arg "Record types unsupported"
           | Ptype_open -> invalid_arg "Open types unsupported") in
 
