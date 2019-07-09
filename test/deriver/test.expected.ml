004c4846cc631543ffc3aefb17bfe9785ff559c4
Craig Ferguson <cf443@cam.ac.uk>
2019-07-10 00:29:59 +0200

    feat: add support for (non-mutual) recursive types

--- test/deriver/test.expected.ml
+++ test/deriver/test.expected.ml
@@ -1,58 +1,63 @@
 type test_unit = unit[@@deriving irmin]
-let test_unit = Irmin.Type.unit
+let test_unit_t = Irmin.Type.unit
 type test_bool = bool[@@deriving irmin]
-let test_bool = Irmin.Type.bool
+let test_bool_t = Irmin.Type.bool
 type test_char = char[@@deriving irmin]
-let test_char = Irmin.Type.char
+let test_char_t = Irmin.Type.char
 type test_int32 = int32[@@deriving irmin]
-let test_int32 = Irmin.Type.int32
+let test_int32_t = Irmin.Type.int32
 type test_int64 = int64[@@deriving irmin]
-let test_int64 = Irmin.Type.int64
+let test_int64_t = Irmin.Type.int64
 type test_float = float[@@deriving irmin]
-let test_float = Irmin.Type.float
+let test_float_t = Irmin.Type.float
 type test_string = string[@@deriving irmin]
-let test_string = Irmin.Type.string
+let test_string_t = Irmin.Type.string
 type test_list1 = string list[@@deriving irmin]
-let test_list1 = let open Irmin.Type in list string
+let test_list1_t = let open Irmin.Type in list string
 type test_list2 = int32 list list list[@@deriving irmin]
-let test_list2 = let open Irmin.Type in list (list (list int32))
+let test_list2_t = let open Irmin.Type in list (list (list int32))
 type test_array = bool array[@@deriving irmin]
-let test_array = let open Irmin.Type in array bool
+let test_array_t = let open Irmin.Type in array bool
 type test_option = unit option[@@deriving irmin]
-let test_option = let open Irmin.Type in option unit
+let test_option_t = let open Irmin.Type in option unit
 type test_pair = (string * int32)[@@deriving irmin]
-let test_pair = let open Irmin.Type in pair string int32
+let test_pair_t = let open Irmin.Type in pair string int32
 type test_triple = (string * int32 * bool)[@@deriving irmin]
-let test_triple = let open Irmin.Type in triple string int32 bool
+let test_triple_t = let open Irmin.Type in triple string int32 bool
 type test_result = (int32, string) result[@@deriving irmin]
-let test_result = let open Irmin.Type in result int32 string
+let test_result_t = let open Irmin.Type in result int32 string
+type test_alias = test_result[@@deriving irmin]
+let test_alias_t = test_result_t
+type t = test_alias
+module S = struct type nonrec t = t list end
 type test_variant1 =
   | A [@@deriving irmin]
-let test_variant1 =
+let test_variant1_t =
   let open Irmin.Type in
     ((variant "test_variant1" (fun a -> function | A -> a)) |~ (case0 "A" A))
       |> sealv
 type test_variant2 =
   | A2 of int64 [@@deriving irmin]
-let test_variant2 =
+let test_variant2_t =
   let open Irmin.Type in
     ((variant "test_variant2" (fun a2 -> function | A2 x1 -> a2 x1)) |~
        (case1 "A2" int64 (fun x1 -> A2 x1)))
       |> sealv
 type test_variant3 =
   | A3 of string * test_variant2 [@@deriving irmin]
-let test_variant3 =
+let test_variant3_t =
   let open Irmin.Type in
     ((variant "test_variant3"
         (fun a3 -> function | A3 (x1, x2) -> a3 (x1, x2)))
        |~
-       (case1 "A3" (pair string test_variant2) (fun (x1, x2) -> A3 (x1, x2))))
+       (case1 "A3" (pair string test_variant2_t)
+          (fun (x1, x2) -> A3 (x1, x2))))
       |> sealv
 type test_variant4 =
   | A4 
   | B4 
   | C4 [@@deriving irmin]
-let test_variant4 =
+let test_variant4_t =
   let open Irmin.Type in
     ((((variant "test_variant4"
           (fun a4 ->
@@ -65,7 +70,7 @@ type test_variant5 =
   | A5 
   | B5 of string * test_variant4 
   | C5 of int32 * string * unit [@@deriving irmin]
-let test_variant5 =
+let test_variant5_t =
   let open Irmin.Type in
     ((((variant "test_variant5"
           (fun a5 ->
@@ -77,37 +82,66 @@ let test_variant5 =
                  | C5 (x1, x2, x3) -> c5 (x1, x2, x3)))
          |~ (case0 "A5" A5))
         |~
-        (case1 "B5" (pair string test_variant4) (fun (x1, x2) -> B5 (x1, x2))))
+        (case1 "B5" (pair string test_variant4_t)
+           (fun (x1, x2) -> B5 (x1, x2))))
        |~
        (case1 "C5" (triple int32 string unit)
           (fun (x1, x2, x3) -> C5 (x1, x2, x3))))
       |> sealv
+type test_variant6 =
+  | Nil 
+  | Cons of string * test_variant6 [@@deriving irmin]
+let test_variant6_t =
+  Irmin.Type.mu
+    (fun test_variant6_t ->
+       let open Irmin.Type in
+         (((variant "test_variant6"
+              (fun nil ->
+                 fun cons ->
+                   function | Nil -> nil | Cons (x1, x2) -> cons (x1, x2)))
+             |~ (case0 "Nil" Nil))
+            |~
+            (case1 "Cons" (pair string test_variant6_t)
+               (fun (x1, x2) -> Cons (x1, x2))))
+           |> sealv)
 type test_record1 = {
   alpha: string ;
   beta: int64 list ;
   gamma: test_variant5 }[@@deriving irmin]
-let test_record1 =
+let test_record1_t =
   let open Irmin.Type in
-    ((((record "irmin"
+    ((((record "test_record1"
           (fun alpha -> fun beta -> fun gamma -> { alpha; beta; gamma }))
          |+ (field "alpha" string (fun t -> t.alpha)))
         |+ (field "beta" (list int64) (fun t -> t.beta)))
-       |+ (field "gamma" test_variant5 (fun t -> t.gamma)))
+       |+ (field "gamma" test_variant5_t (fun t -> t.gamma)))
       |> sealr
 type test_record2 =
   {
   the_FIRST_identifier: test_record1 option ;
   the_SECOND_identifier: (string, int32) result list }[@@deriving irmin]
-let test_record2 =
+let test_record2_t =
   let open Irmin.Type in
-    (((record "irmin"
+    (((record "test_record2"
          (fun the_FIRST_identifier ->
             fun the_SECOND_identifier ->
               { the_FIRST_identifier; the_SECOND_identifier }))
         |+
-        (field "the_FIRST_identifier" (option test_record1)
+        (field "the_FIRST_identifier" (option test_record1_t)
            (fun t -> t.the_FIRST_identifier)))
        |+
        (field "the_SECOND_identifier" (list (result string int32))
           (fun t -> t.the_SECOND_identifier)))
       |> sealr
+module type S  =
+  sig
+    type t = string[@@deriving irmin]
+    val t : t Irmin.Type.t
+    type my_int = (int32 * t)[@@deriving irmin]
+    val my_int_t : my_int Irmin.Type.t
+    type my_variant =
+      | A of (my_int, int) result 
+      | B of unit 
+      | C of string * int32 [@@deriving irmin]
+    val my_variant_t : my_variant Irmin.Type.t
+  end
--- test/deriver/test.ml
+++ test/deriver/test.ml
@@ -1,53 +1,50 @@
+(* Tests of the basic types *)
 type test_unit = unit [@@deriving irmin]
-
 type test_bool = bool [@@deriving irmin]
-
 type test_char = char [@@deriving irmin]
-
 type test_int32 = int32 [@@deriving irmin]
-
 type test_int64 = int64 [@@deriving irmin]
-
 type test_float = float [@@deriving irmin]
-
 type test_string = string [@@deriving irmin]
 
+(* Tests of the composite type combinators *)
 type test_list1 = string list [@@deriving irmin]
-
 type test_list2 = int32 list list list [@@deriving irmin]
-
 type test_array = bool array [@@deriving irmin]
-
 type test_option = unit option [@@deriving irmin]
-
 type test_pair = (string) * int32 [@@deriving irmin]
-
 type test_triple = string * int32 * bool [@@deriving irmin]
-
 type test_result = (int32, string) result [@@deriving irmin]
 
+(* Miscellaneous *)
+type test_alias = test_result [@@deriving irmin]
+type t = test_alias
+module S = struct
+  type nonrec t = t list
+end
+
+(* Variants *)
 type test_variant1 = A [@@deriving irmin]
-
 type test_variant2 = A2 of int64 [@@deriving irmin]
-
 type test_variant3 = A3 of string * test_variant2 [@@deriving irmin]
-
 type test_variant4 =
   | A4
   | B4
   | C4 [@@deriving irmin]
-
 type test_variant5 =
   | A5
   | B5 of string * test_variant4
   | C5 of int32 * string * unit [@@deriving irmin]
+type test_variant6 =
+  | Nil
+  | Cons of string * test_variant6 [@@deriving irmin]
 
+(* Records *)
 type test_record1 = {
   alpha: string;
   beta: int64 list;
   gamma: test_variant5
 } [@@deriving irmin]
-
 type test_record2 = {
   the_FIRST_identifier: test_record1 option;
   the_SECOND_identifier: (string, int32) result list;
@@ -55,7 +52,9 @@ type test_record2 = {
 
 (* Tests of the signature deriver *)
 module type S = sig
-  type my_int = int32 [@@deriving irmin]
+  type t = string [@@deriving irmin]
+
+  type my_int = int32 * t [@@deriving irmin]
 
   type my_variant =
     | A of (my_int, int) result
