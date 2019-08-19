8b4c66a13584a8a5cf104b877c5878fff674a91f
Thomas Gazagnaire <thomas@gazagnaire.org>
2019-08-19 11:35:48 +0200

    irmin-pack: bigger constants

--- src/irmin-pack/irmin_pack.ml
+++ src/irmin-pack/irmin_pack.ml
@@ -26,11 +26,11 @@ let fresh_key =
 
 let lru_size_key =
   Irmin.Private.Conf.key ~doc:"Size of the LRU cache for pack entries."
-    "lru-size" Irmin.Private.Conf.int 10_000
+    "lru-size" Irmin.Private.Conf.int 100_000
 
 let index_log_size_key =
   Irmin.Private.Conf.key ~doc:"Size of index logs." "index-log-size"
-    Irmin.Private.Conf.int 10_000
+    Irmin.Private.Conf.int 500_000
 
 let readonly_key =
   Irmin.Private.Conf.key ~doc:"Start with a read-only disk." "readonly"
@@ -61,7 +61,7 @@ let root config =
   | Some r -> r
 
 let config ?(fresh = false) ?(shared = true) ?(readonly = false)
-    ?(lru_size = 10_000) ?(index_log_size = 10_000) root =
+    ?(lru_size = 100_000) ?(index_log_size = 500_000) root =
   let config = Irmin.Private.Conf.empty in
   let config = Irmin.Private.Conf.add config fresh_key fresh in
   let config = Irmin.Private.Conf.add config root_key (Some root) in
