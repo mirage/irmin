fdc7e10f6157b0f7c74715592190b1e78ab52c84
Craig Ferguson <me@craigfe.io>
2020-07-10 16:28:12 +0200

    bench: add benchmarks for short_hash and pre_hash

--- bench/irmin/main.ml
+++ bench/irmin/main.ml
@@ -1,39 +1,66 @@
 open Bechamel
 open Toolkit
 module T = Irmin.Type
-module H = Irmin.Hash.SHA1
+module Hash = Irmin.Hash.BLAKE2B
 
-module Generic_codec = struct
-  type t =
-    | G : {
-        name : string;
+let ( >> ) f g x = g (f x)
+
+module Generic_op = struct
+  type op =
+    | Consumer : { consume : 'a. 'a T.t -> [ `Staged of 'a -> _ ] } -> op
+    | Codec : {
         encode : 'a. 'a T.t -> [ `Staged of 'a -> string ];
         decode : 'a. 'a T.t -> [ `Staged of string -> 'a ];
       }
-        -> t
+        -> op
+
+  type t = { name : string; operation : op }
 
   let bin_string : t =
-    let encode ty = `Staged (T.to_bin_string ty) in
-    let decode ty =
-      let f = T.of_bin_string ty in
-      `Staged (fun s -> f s |> Result.get_ok)
+    let encode (type a) (ty : a T.t) =
+      let f = T.to_bin_string ty in
+      `Staged (f : a -> string)
     in
-    G { name = "bin_string"; encode; decode }
+    let decode (type a) (ty : a T.t) =
+      let f = T.of_bin_string ty in
+      `Staged (f >> Result.get_ok : string -> a)
+    in
+    { name = "bin_string"; operation = Codec { encode; decode } }
 
   let bin : t =
-    let encode ty =
+    let encode (type a) (ty : a T.t) =
       let f = T.encode_bin ty ?headers:None in
       `Staged
-        (fun s ->
-          let buffer = Buffer.create 0 in
-          f s (Buffer.add_string buffer);
-          Buffer.contents buffer)
+        ( fun a ->
+            let buffer = Buffer.create 0 in
+            f a (Buffer.add_string buffer);
+            Buffer.contents buffer
+          : a -> string )
     in
-    let decode ty =
+    let decode (type a) (ty : a T.t) =
       let f = T.decode_bin ty ?headers:None in
-      `Staged (fun s -> f s 0 |> snd)
+      `Staged (fun s -> f s 0 |> snd : string -> a)
     in
-    G { name = "bin"; encode; decode }
+    { name = "bin"; operation = Codec { encode; decode } }
+
+  let pre_hash : t =
+    let consume (type a) (ty : a T.t) =
+      let f = T.pre_hash ty in
+      `Staged
+        ( fun a ->
+            let buffer = Buffer.create 0 in
+            f a (Buffer.add_string buffer);
+            Buffer.contents buffer
+          : a -> string )
+    in
+    { name = "pre_hash"; operation = Consumer { consume } }
+
+  let short_hash : t =
+    let consume (type a) (ty : a T.t) =
+      let f = T.short_hash ty in
+      `Staged (f : a -> int)
+    in
+    { name = "short_hash"; operation = Consumer { consume } }
 end
 
 (** Generators for random data *)
@@ -61,28 +88,37 @@ module Faker = struct
   let triple a b c () = (a (), b (), c ())
 end
 
-(** Arrays of structured random data for use in the benchmarks below *)
 let nb_runs = 30_000
 
 module Data = struct
   type t =
-    | E : { typ : 'a T.t; unencoded : 'a array; encoded : string array } -> t
+    | E : {
+        typ : 'a T.t;
+        random : 'a array;
+        encoded : string array option;
+            (** For operations with a [decode] action *)
+      }
+        -> t
 
-  module Generate (Codec : sig
-    val t : Generic_codec.t
+  (** Given a generic operation, generate arrays of {!nb_runs} elements needed
+      to benchmark it on each type we care about. *)
+  module Generate (Op : sig
+    val t : Generic_op.op
   end) =
   struct
     let mk_data (type a) (typ : a T.t) faker =
-      let (G { encode; _ }) = Codec.t in
-      let unencoded = Array.init nb_runs (fun _ -> faker ()) in
+      let random = Array.init nb_runs (fun _ -> faker ()) in
       let encoded =
-        let (`Staged f) = encode typ in
-        unencoded |> Array.map f
+        match Op.t with
+        | Consumer _ -> None
+        | Codec { encode; _ } ->
+            let (`Staged f) = encode typ in
+            Some (random |> Array.map f)
       in
-      E { typ; unencoded; encoded }
+      E { typ; random; encoded }
 
     let hash =
-      mk_data H.t (fun () -> H.hash (fun f -> f (Faker.string 1024 ())))
+      mk_data Hash.t (fun () -> Hash.hash (fun f -> f (Faker.string 1024 ())))
 
     let string_1024 = mk_data T.string Faker.(string 1024)
 
@@ -108,58 +144,61 @@ module Data = struct
   end
 end
 
-let run_encode (Generic_codec.G { encode; _ }) typ arr =
+(** Stage a function with inputs chosen sequentially from an array of size
+    {!nb_runs}. *)
+let distrib_arr (type a b) (`Staged (f : a -> b)) (arr : a array) :
+    (unit -> unit) Staged.t =
   let i = ref 0 in
-  let (`Staged f) = encode typ in
   Staged.stage @@ fun () ->
-  let _ = f arr.(!i mod nb_runs) in
+  let (_ : b) = f arr.(!i mod nb_runs) in
   incr i
 
-let run_decode (Generic_codec.G { decode; _ }) typ arr =
-  let i = ref 0 in
-  let (`Staged f) = decode typ in
-  Staged.stage @@ fun () ->
-  let _ = f arr.(!i mod nb_runs) in
-  incr i
+let test ~op ~name (E data : Data.t) =
+  let tests =
+    match op.Generic_op.operation with
+    | Codec { encode; decode } ->
+        [
+          Test.make ~name:"enc" (distrib_arr (encode data.typ) data.random);
+          Test.make ~name:"dec"
+            (distrib_arr (decode data.typ) (data.encoded |> Option.get));
+        ]
+    | Consumer { consume } ->
+        [ Test.make ~name:"" (distrib_arr (consume data.typ) data.random) ]
+  in
 
-(** Benchmark an encoder/decoder pair *)
-let test_codec ~generic_codec:f ~name (E data : Data.t) =
-  Test.make_grouped ~name
-    [
-      Test.make ~name:"enc" (run_encode f data.typ data.unencoded);
-      Test.make ~name:"dec" (run_decode f data.typ data.encoded);
-    ]
+  Test.make_grouped ~name tests
 
-let test_generic_codec ~name
-    (Generic_codec.G { name = fname; _ } as generic_codec) =
-  Fmt.epr "Generating data for `%s' codec ...%!" fname;
+let test_operation ~name (op : Generic_op.t) =
+  Fmt.epr "Generating data for `%s' codec ...%!" op.name;
   let module Data = Data.Generate (struct
-    let t = generic_codec
+    let t = op.operation
   end) in
   Fmt.epr " Done!\n%!";
-  let test_codec = test_codec ~generic_codec in
+  let test = test ~op in
   Test.make_grouped ~name
     [
       Test.make_grouped ~name:"int"
         [
-          test_codec ~name:"char (0 <= b <= 7)" Data.char_int;
-          test_codec ~name:"short (8 <= b <= 15)" Data.short_int;
-          test_codec ~name:"long (16 <= b <= 32)" Data.long_int;
+          test ~name:"char (0 <= b <= 7)" Data.char_int;
+          test ~name:"short (8 <= b <= 15)" Data.short_int;
+          test ~name:"long (16 <= b <= 32)" Data.long_int;
         ];
-      test_codec ~name:"hash" Data.hash;
-      test_codec ~name:"string<1024>" Data.string_1024;
-      test_codec ~name:"bytes<1024>" Data.bytes_1024;
-      test_codec ~name:"int32" Data.int32;
-      test_codec ~name:"int64" Data.int64;
-      test_codec ~name:"pair_string_int" Data.pair_string_int;
-      test_codec ~name:"triple_short_int" Data.triple_short_int;
+      test ~name:"hash" Data.hash;
+      test ~name:"string<1024>" Data.string_1024;
+      test ~name:"bytes<1024>" Data.bytes_1024;
+      test ~name:"int32" Data.int32;
+      test ~name:"int64" Data.int64;
+      test ~name:"pair_string_int" Data.pair_string_int;
+      test ~name:"triple_short_int" Data.triple_short_int;
     ]
 
 let suite =
   Test.make_grouped ~name:""
     [
-      test_generic_codec ~name:"bin" Generic_codec.bin;
-      test_generic_codec ~name:"str" Generic_codec.bin_string;
+      test_operation ~name:"bin" Generic_op.bin;
+      test_operation ~name:"bin_string" Generic_op.bin_string;
+      test_operation ~name:"short_hash" Generic_op.short_hash;
+      test_operation ~name:"pre_hash" Generic_op.pre_hash;
     ]
 
 let benchmark () =
