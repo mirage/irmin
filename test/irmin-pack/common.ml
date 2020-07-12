477b1db1aadee5d5b7b2a500ae8400fe50a7be61
Thomas Gazagnaire <thomas@gazagnaire.org>
2020-07-12 18:22:55 +0200

    irmin-pack: add (failing) tests for Pack.close

--- test/irmin-pack/common.ml
+++ test/irmin-pack/common.ml
@@ -66,13 +66,13 @@ struct
 
   let log_size = 10_000_000
 
-  let get_pack () =
+  let get_pack ?(lru_size = 0) () =
     let name = fresh_name "dict" in
     let f = ref (fun () -> ()) in
     let index =
       Index.v ~auto_flush_callback:(fun () -> !f ()) ~log_size ~fresh:true name
     in
-    Pack.v ~fresh:true ~lru_size:0 ~index name >|= fun pack ->
+    Pack.v ~fresh:true ~lru_size ~index name >|= fun pack ->
     (f := fun () -> Pack.flush ~index:false pack);
     let clone_pack ~readonly = Pack.v ~fresh:false ~readonly ~index name in
     let clone_index_pack ~readonly =
--- test/irmin-pack/test_pack.mli
+++ test/irmin-pack/test_pack.mli
@@ -1,3 +1,3 @@
 val suite : Irmin_test.t
 
-val misc : string * unit Alcotest.test_case list
+val misc : (string * unit Alcotest.test_case list) list
