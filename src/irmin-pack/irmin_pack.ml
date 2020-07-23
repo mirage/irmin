3d721ad640f692f5765abb4ee7ca4b56ea2c9915
icristescu <domninac@gmail.com>
2020-07-23 17:38:45 +0200

    Expose throttle from index

--- src/irmin-pack/irmin_pack.ml
+++ src/irmin-pack/irmin_pack.ml
@@ -28,7 +28,25 @@ module Default = struct
   let index_log_size = 500_000
 
   let readonly = false
+
+  let index_throttle = `Block_writes
 end
+
+let throttle_converter =
+  let parse = function
+    | "Block_writes" -> Ok `Block_writes
+    | "Overcommit_memory" -> Ok `Overcommit_memory
+    | s ->
+        Error
+          (`Msg
+            (Printf.sprintf
+               "invalid %s, expected one of: Block_writes Overcommit_memory" s))
+  in
+  let print pp = function
+    | `Block_writes -> Fmt.string pp "Block_writes"
+    | `Overcommit_memory -> Fmt.string pp "Overcommit_memory"
+  in
+  (parse, print)
 
 let fresh_key =
   Irmin.Private.Conf.key ~doc:"Start with a fresh disk." "fresh"
@@ -46,6 +64,11 @@ let readonly_key =
   Irmin.Private.Conf.key ~doc:"Start with a read-only disk." "readonly"
     Irmin.Private.Conf.bool Default.readonly
 
+let index_throttle_key =
+  Irmin.Private.Conf.key
+    ~doc:"Strategy to use for large writes when index caches are full."
+    "index-throttle" throttle_converter Default.index_throttle
+
 let fresh config = Irmin.Private.Conf.get config fresh_key
 
 let lru_size config = Irmin.Private.Conf.get config lru_size_key
@@ -53,6 +76,8 @@ let lru_size config = Irmin.Private.Conf.get config lru_size_key
 let readonly config = Irmin.Private.Conf.get config readonly_key
 
 let index_log_size config = Irmin.Private.Conf.get config index_log_size_key
+
+let index_throttle config = Irmin.Private.Conf.get config index_throttle_key
 
 let root_key = Irmin.Private.Conf.root
 
@@ -63,7 +88,7 @@ let root config =
 
 let config ?(fresh = Default.fresh) ?(readonly = Default.readonly)
     ?(lru_size = Default.lru_size) ?(index_log_size = Default.index_log_size)
-    root =
+    ?(index_throttle = Default.index_throttle) root =
   let config = Irmin.Private.Conf.empty in
   let config = Irmin.Private.Conf.add config fresh_key fresh in
   let config = Irmin.Private.Conf.add config root_key (Some root) in
@@ -72,6 +97,9 @@ let config ?(fresh = Default.fresh) ?(readonly = Default.readonly)
     Irmin.Private.Conf.add config index_log_size_key index_log_size
   in
   let config = Irmin.Private.Conf.add config readonly_key readonly in
+  let config =
+    Irmin.Private.Conf.add config index_throttle_key index_throttle
+  in
   config
 
 let ( ++ ) = Int64.add
@@ -470,12 +498,13 @@ struct
         let lru_size = lru_size config in
         let readonly = readonly config in
         let log_size = index_log_size config in
+        let throttle = index_throttle config in
         let f = ref (fun () -> ()) in
         let index =
           Index.v
             ~auto_flush_callback:(fun () -> !f ())
               (* backpatching to add pack flush before an index flush *)
-            ~fresh ~readonly ~log_size root
+            ~fresh ~readonly ~throttle ~log_size root
         in
         Contents.CA.v ~fresh ~readonly ~lru_size ~index root >>= fun contents ->
         Node.CA.v ~fresh ~readonly ~lru_size ~index root >>= fun node ->
