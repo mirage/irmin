675a68cba29ecd2159e1962c85ddca810450f9b5
Craig Ferguson <me@craigfe.io>
2020-07-27 16:05:38 +0200

    irmin-pack: reshuffle data locations for the migration test

--- test/irmin-pack/migration.ml
+++ test/irmin-pack/migration.ml
@@ -17,18 +17,25 @@ let config ?(readonly = false) ?(fresh = true) root =
 
 let info () = Irmin.Info.empty
 
-let rootV1 = Filename.concat "_build" "testV1"
+let root_V1_source, root_V1, root_V2 =
+  let ( / ) = Filename.concat in
+  ( "test" / "irmin-pack" / "data" / "version_1",
+    "_build" / "version_1",
+    "_build" / "version_2" )
 
 (** testV1 is a store in V1 used to test the migration to V2 *)
 let setup_test_env () =
-  let rootV2 = Filename.concat "_build" "_v2" in
-  let root_testV1 = "testV1" in
-  rm_dir rootV1;
-  rm_dir rootV2;
-  let cmd = Printf.sprintf "cp -r %s %s" root_testV1 rootV1 in
+  rm_dir root_V1;
+  rm_dir root_V2;
+  let cmd = Printf.sprintf "cp -r %s %s" root_V1_source root_V1 in
   Fmt.epr "exec: %s\n%!" cmd;
-  let _ = Sys.command cmd in
-  ()
+  match Sys.command cmd with
+  | 0 -> ()
+  | n ->
+      Fmt.failwith
+        "Failed to set up the test environment: command `%s' exited with \
+         non-zero exit code %d"
+        cmd n
 
 (** Opening a store in V1 automatically migrates it to V2. Old values are
     readable and new values can be added. Reopening the store in RO mode is also
@@ -54,14 +61,14 @@ let test () =
     | None -> Alcotest.failf "branch foo not found"
     | Some commit -> check r commit "check old values b" [ "b" ] "y"
   in
-  S.Repo.v (config ~readonly:false ~fresh:false rootV1) >>= fun r ->
+  S.Repo.v (config ~readonly:false ~fresh:false root_V1) >>= fun r ->
   check_old_values r >>= fun () ->
   S.Tree.add S.Tree.empty [ "c" ] "x" >>= fun tree ->
   S.Commit.v r ~parents:[] ~info:(info ()) tree >>= fun c ->
   check r c "check new values" [ "c" ] "x" >>= fun () ->
   S.Repo.close r >>= fun () ->
-  S.Repo.v (config ~readonly:false ~fresh:false rootV1) >>= fun r ->
-  S.Repo.v (config ~readonly:true ~fresh:false rootV1) >>= fun ro ->
+  S.Repo.v (config ~readonly:false ~fresh:false root_V1) >>= fun r ->
+  S.Repo.v (config ~readonly:true ~fresh:false root_V1) >>= fun ro ->
   check_old_values ro >>= fun () ->
   check ro c "check new values after reopening" [ "c" ] "x" >>= fun () ->
   S.Repo.close r >>= fun () -> S.Repo.close ro
