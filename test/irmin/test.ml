385dc0c71b337d4cb1204b8b6935760c696b92cc
Craig Ferguson <me@craigfe.io>
2020-11-20 16:43:50 +0100

    irmin: add expect tests for short_hash of Hash.BLAKE2B

--- test/irmin/test.ml
+++ test/irmin/test.ml
@@ -1,3 +1,3 @@
-let suite = [ ("tree", Test_tree.suite) ]
+let suite = [ ("tree", Test_tree.suite); ("hash", Test_hash.suite) ]
 
 let () = Lwt_main.run (Alcotest_lwt.run "irmin" suite)
---
+++ test/irmin/test_hash.ml
@@ -0,0 +1,19 @@
+open Irmin
+
+let test_short_hash () =
+  let h = Hash.BLAKE2B.hash (fun f -> f "") in
+  let () =
+    Hash.BLAKE2B.short_hash h
+    |> Alcotest.(check int) "Specialised short hash" 241225442164632184
+  in
+  let () =
+    Type.(unstage (short_hash Hash.BLAKE2B.t)) ~seed:0 h
+    |> Alcotest.(check int) "Generic seeded short hash" 674923654
+  in
+  let () =
+    Type.(unstage (short_hash Hash.BLAKE2B.t)) ?seed:None h
+    |> Alcotest.(check int) "Generic unseeded short hash" 674923654
+  in
+  ()
+
+let suite = [ Alcotest_lwt.test_case_sync "short_hash" `Quick test_short_hash ]
