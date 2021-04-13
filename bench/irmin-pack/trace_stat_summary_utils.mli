d2bb8171f1d834fd52b0d275b9e8c388ee0ba44d
Ngoguey42 <ngoguey@student.42.fr>
2021-04-13 17:02:09 +0200

    irmin-bench: CLI and PP for stat trace

--- bench/irmin-pack/trace_stat_summary_utils.mli
+++ bench/irmin-pack/trace_stat_summary_utils.mli
@@ -6,6 +6,32 @@
 type histo = (float * int) list [@@deriving repr]
 type curve = float list [@@deriving repr]
 
+val create_pp_real :
+  ?significant_digits:int -> float list -> Format.formatter -> float -> unit
+(** [create_pp_real examples] is [pp_real], a float pretty-printer that adapts
+    to the [examples] shown to it.
+
+    It is highly recommended, but not mandatory, for all the numbers passed to
+    [pp_real] to be included [examples].
+
+    [significant_digits] is used to snap certain numbers to the nearest integer. *)
+
+val create_pp_seconds : float list -> Format.formatter -> float -> unit
+(** [create_pp_seconds examples] is [pp_seconds], a time span pretty-printer
+    that adapts to the [examples] shown to it.
+
+    It is highly recommended, but not mandatory, for all the numbers passed to
+    [pp_seconds] to be included [examples]. *)
+
+val pp_percent : Format.formatter -> float -> unit
+(** Pretty prints a percent in a way that always takes 4 chars.
+
+    Examples: [0.] is ["  0%"], [0.00001] is ["0.0%"], [0.003] is ["0.3%"],
+    [0.15] is [" 15%"], [9.0] is [900%], [700.] is [700x], [410_000.0] is
+    ["4e6x"] and [1e100] is ["++++"].
+
+    Negative inputs are undefined. *)
+
 (** Functional Exponential Moving Average (EMA). *)
 module Exponential_moving_average : sig
   type t
---
+++ bench/irmin-pack/trace_stats.ml
@@ -0,0 +1,185 @@
+(*
+ * Copyright (c) 2018-2021 Tarides <contact@tarides.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+(** [trace_stats.exe].
+
+    This file is NOT meant to be used from Tezos, as opposed to some other
+    "trace_*" files. *)
+
+open Irmin_traces
+module Def = Trace_definitions.Stat_trace
+module Summary = Trace_stat_summary
+
+let is_trace_magic s = Trace_common.Magic.to_string Def.magic = s
+
+let summarise path =
+  Summary.(summarise path |> Fmt.pr "%a\n" (Irmin.Type.pp_json t))
+
+let class_of_path p =
+  let chan = open_in_bin p in
+  if in_channel_length chan < 8 then
+    Fmt.invalid_arg "File \"%s\" should be a stat trace or a json." p;
+  let magic = really_input_string chan 8 in
+  close_in chan;
+  if is_trace_magic magic then
+    let block_count =
+      Def.open_reader p
+      |> snd
+      |> Seq.fold_left
+           (fun count op -> match op with `Commit _ -> count + 1 | _ -> count)
+           0
+    in
+    `Trace block_count
+  else
+    let chan = open_in_bin p in
+    let raw = really_input_string chan (in_channel_length chan) in
+    close_in chan;
+    match Irmin.Type.of_json_string Summary.t raw with
+    | Error (`Msg msg) ->
+        Fmt.invalid_arg
+          "File \"%s\" should be a stat trace or a json.\nError: %s" p msg
+    | Ok s -> `Summary s
+
+let pp name_per_path paths cols_opt =
+  let class_per_path = List.map class_of_path paths in
+  let block_count =
+    (* When pretty printing, all the summaries will have to have the same number of
+       blocks, i.e. the smallest of all inputs. *)
+    let trace_lengths =
+      List.filter_map
+        (function `Trace v -> Some v | `Summary _ -> None)
+        class_per_path
+    in
+    let summary_lengths =
+      List.filter_map
+        (function `Trace _ -> None | `Summary s -> Some s.Summary.block_count)
+        class_per_path
+    in
+    let s = List.length summary_lengths > 0 in
+    let t = List.length trace_lengths > 0 in
+    let min_trace_length = List.fold_left min max_int trace_lengths in
+    let min_summary_length = List.fold_left min max_int summary_lengths in
+    let max_summary_length = List.fold_left max 0 summary_lengths in
+    if s && min_summary_length <> max_summary_length then
+      invalid_arg
+        "Can't pretty print 2 summaries with a different number of blocks.";
+    if s && t && min_summary_length > min_trace_length then
+      invalid_arg
+        "Can't pretty print a trace alongside a summary that has a higher \
+         block count.";
+    min min_trace_length min_summary_length
+  in
+  let summaries =
+    List.map2
+      (fun path -> function
+        | `Summary s -> s
+        | `Trace _ -> Summary.summarise ~block_count path)
+      paths class_per_path
+  in
+  let col_count =
+    match cols_opt with
+    | Some v -> v
+    | None -> if List.length summaries > 1 then 4 else 5
+  in
+  Fmt.pr "%a\n" (Trace_stat_summary_pp.pp col_count) (name_per_path, summaries)
+
+let pp paths named_paths cols_opt =
+  let name_per_path, paths =
+    List.mapi (fun i v -> (string_of_int i, v)) paths @ named_paths
+    |> List.split
+  in
+  if List.length paths = 0 then
+    invalid_arg "trace_stats.exe pp: At least one path should be provided";
+  pp name_per_path paths cols_opt
+
+open Cmdliner
+
+let term_summarise =
+  let stat_trace_file =
+    let doc = Arg.info ~docv:"PATH" ~doc:"A stat trace file" [] in
+    Arg.(required @@ pos 0 (some string) None doc)
+  in
+  Term.(const summarise $ stat_trace_file)
+
+let term_pp =
+  let arg_indexed_files =
+    let open Arg in
+    let a = pos_all non_dir_file [] (info [] ~docv:"FILE") in
+    value a
+  in
+  let arg_named_files =
+    let open Arg in
+    let a =
+      opt_all (pair string non_dir_file) []
+        (info [ "f"; "named-file" ]
+           ~doc:
+             "A comma-separated pair of short name / path to trace or summary. \
+              The short name is used to tag the rows inside the pretty printed \
+              table.")
+    in
+    value a
+  in
+  let arg_columns =
+    let open Arg in
+    let doc =
+      Arg.info ~doc:"Number of sample columns to show." [ "c"; "columns" ]
+    in
+    let a = opt (some int) None doc in
+    value a
+  in
+  Term.(const pp $ arg_indexed_files $ arg_named_files $ arg_columns)
+
+let () =
+  let man = [] in
+  let i =
+    Term.info ~man ~doc:"Processing of stat traces and stat trace summaries."
+      "trace_stats"
+  in
+
+  let man =
+    [
+      `P "From stat trace (repr) to summary (json).";
+      `S "EXAMPLE";
+      `P "trace_stats.exe summarise run0.repr > run0.json";
+    ]
+  in
+  let j = Term.info ~man ~doc:"Stat Trace to Summary" "summarise" in
+
+  let man =
+    [
+      `P
+        "Accepts both stat traces (repr) and summaries (json). The file type \
+         is automatically infered.";
+      `P
+        "When a single file is provided, a subset of the summary of that file \
+         is computed and shown.";
+      `P
+        "When multiple files are provided, a subset of the summary of each \
+         file is computed and shown in a way that makes comparisons between \
+         files easy.";
+      `S "EXAMPLES";
+      `P "trace_stats.exe pp run0.json";
+      `Noblank;
+      `P "trace_stats.exe pp run1.repr";
+      `Noblank;
+      `P "trace_stats.exe pp run0.json run1.repr run3.json";
+      `Noblank;
+      `P "trace_stats.exe pp -f r0,run0.json -f r1,run1.repr";
+    ]
+  in
+  let k = Term.info ~man ~doc:"Comparative Pretty Printing" "pp" in
+  Term.exit
+  @@ Term.eval_choice (term_summarise, i) [ (term_summarise, j); (term_pp, k) ]
