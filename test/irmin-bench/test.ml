01000b2685a0b7ef56163a4d9cbb56579ef78b00
Ngoguey42 <ngoguey@student.42.fr>
2021-05-07 16:46:33 +0200

    irmin-bench: Replay trace in unit tests

--- test/irmin-bench/test.ml
+++ test/irmin-bench/test.ml
@@ -14,4 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-let () = Alcotest.run "irmin-bench" (Ema.test_cases @ Misc.test_cases)
+let () =
+  Alcotest.run "irmin-bench"
+    (Ema.test_cases @ Misc.test_cases @ Replay.test_cases)
