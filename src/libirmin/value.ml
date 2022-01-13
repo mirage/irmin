8fe14c945683477913d89836022c33d63b459ba2
zach <zachshipko@gmail.com>
2022-01-12 21:20:54 -0800

    Remove extra list/array functions

--- src/libirmin/value.ml
+++ src/libirmin/value.ml
@@ -62,79 +62,35 @@ module Make (I : Cstubs_inverted.INTERNAL) = struct
         Root.create_value (string_from_ptr s ~length))
 
   let () =
-    fn "value_list_new"
-      (void @-> returning value)
-      (fun () -> Root.create_value [])
+    fn "value_array"
+      (ptr value @-> uint64_t @-> returning value)
+      (fun arr n ->
+        catch' value (fun () ->
+            let n = UInt64.to_int n in
+            let a =
+              if is_null arr || n = 0 then [||]
+              else
+                CArray.from_ptr arr n
+                |> CArray.to_list
+                |> List.map Root.get_value
+                |> Array.of_list
+            in
+            Root.create_value a))
 
   let () =
-    fn "value_list_add"
-      (value @-> value @-> returning void)
-      (fun (type a) list x ->
-        let a : a list = Root.get_value list in
-        let b : a = Root.get_value x in
-        Root.set_value list (a @ [ b ]))
-
-  let () =
-    fn "value_list_hd"
-      (value @-> returning value)
-      (fun list ->
-        let list = Root.get_value list in
-        match list with
-        | [] -> null value
-        | list -> Root.create_value (List.hd list))
-
-  let () =
-    fn "value_list_tl"
-      (value @-> returning value)
-      (fun list ->
-        let list = Root.get_value list in
-        Root.create_value (List.tl list))
-
-  let () =
-    fn "value_list_get"
-      (value @-> uint64_t @-> returning value)
-      (fun (type a) arr i ->
-        let i = UInt64.to_int i in
-        let x : a list = Root.get_value arr in
-        Root.create_value (List.nth x i))
-
-  let () =
-    fn "value_list_length"
-      (value @-> returning uint64_t)
-      (fun (type a) x ->
-        let x : a list = Root.get_value x in
-        List.length x |> UInt64.of_int)
-
-  let () =
-    fn "value_array_new"
-      (uint64_t @-> value @-> returning value)
-      (fun i x ->
-        let x = Root.get_value x in
-        Root.create_value (Array.make (UInt64.to_int i) x))
-
-  let () =
-    fn "value_array_set"
-      (value @-> uint64_t @-> value @-> returning void)
-      (fun (type a) arr i x ->
-        let i = UInt64.to_int i in
-        let arr : a array = Root.get_value arr in
-        let x : a = Root.get_value x in
-        arr.(i) <- x)
-
-  let () =
-    fn "value_array_get"
-      (value @-> uint64_t @-> returning value)
-      (fun (type a) arr i ->
-        let i = UInt64.to_int i in
-        let arr : a array = Root.get_value arr in
-        Root.create_value arr.(i))
-
-  let () =
-    fn "value_array_length"
-      (value @-> returning uint64_t)
-      (fun (type a) arr ->
-        let arr : a array = Root.get_value arr in
-        Array.length arr |> UInt64.of_int)
+    fn "value_list"
+      (ptr value @-> uint64_t @-> returning value)
+      (fun arr n ->
+        catch' value (fun () ->
+            let n = UInt64.to_int n in
+            let l =
+              if is_null arr || n = 0 then []
+              else
+                CArray.from_ptr arr n
+                |> CArray.to_list
+                |> List.map Root.get_value
+            in
+            Root.create_value l))
 
   let () =
     fn "value_option"
