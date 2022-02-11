dc5cc72357e4ef94591bd9c84de5765cdc53c9b7
Tom Ridge <tom.j.ridge@googlemail.com>
2022-02-11 17:27:10 +0000

    worker create_sparse_file

--- src/irmin-pack/layers/worker.ml
+++ src/irmin-pack/layers/worker.ml
@@ -28,22 +28,50 @@ let _ = assert(Sys.word_size = 8) (* 64 bit ints *)
     calculates the extents; uses [working_dir] for intermediate results; returns the name
     of the file (which will be within [working_dir]) that holds the extent data; other
     intermediate files are deleted (FIXME not during testing) *)
-let calculate_extents ~working_dir ~reachable_fn =
-  let reachable = Int_mmap.open_ ~fn:reachable_fn ~sz:(File.size reachable_fn / 8) in
-  let chunk_sz = 1_000_000 / 16 in
-  assert(chunk_sz mod 2 = 0); (* needs to be a multiple of 2 *)
-  let sorted_fn = Filename.temp_file ~temp_dir:working_dir "sorted." ".tmp" in
-  let sorted = Int_mmap.open_ ~fn:sorted_fn ~sz:(BA1.dim reachable.Int_mmap.arr) in
-  External_sort.sort ~chunk_sz ~src:reachable.arr ~dst:sorted.arr;
+let calculate_extents ~working_dir ~reachable_fn = 
+  let reachable  = Int_mmap.open_ ~fn:reachable_fn ~sz:(File.size reachable_fn / 8) in
+  let chunk_sz   = 1_000_000 / 16 in
+  let _          = assert(chunk_sz mod 2 = 0) in (* needs to be a multiple of 2 *)
+  let sorted_fn  = Filename.temp_file ~temp_dir:working_dir "sorted." ".tmp" in
+  let sorted     = Int_mmap.open_ ~fn:sorted_fn ~sz:(BA1.dim reachable.Int_mmap.arr) in
+  let _          = External_sort.sort ~chunk_sz ~src:reachable.arr ~dst:sorted.arr in
   let extents_fn = Filename.temp_file ~temp_dir:working_dir "extents." ".tmp" in
-  let extents = Int_mmap.open_ ~fn:extents_fn ~sz:(BA1.dim sorted.Int_mmap.arr) in
-  let n = External_sort.calculate_extents ~src_is_sorted:() ~src:sorted.arr ~dst:extents.arr in
+  let extents    = Int_mmap.open_ ~fn:extents_fn ~sz:(BA1.dim sorted.Int_mmap.arr) in
+  let n          = External_sort.calculate_extents ~src_is_sorted:() ~src:sorted.arr ~dst:extents.arr in
   (* NOTE ftruncate is apparently safe provided we don't try to access past the new end
      via the mmap *)
-  Unix.ftruncate extents.fd (n * 8 (* bytes per int *));
+  Unix.ftruncate extents.fd (n * 8); (* 8 bytes per int *)
   Int_mmap.close reachable;
   Int_mmap.close sorted;
   Int_mmap.close extents;
   extents_fn
+
+
+(* FIXME at the moment we ignore max_pos, which should be the pos at which we split the
+   file *)  
+let create_sparse_file ~(extents_fn:string) ~(src:Pread.t) ~fn : unit = 
+  let _ = 
+    assert(Sys.file_exists extents_fn);
+    assert(File.size extents_fn mod 16 = 0);
+    assert(not (Sys.file_exists fn));
+  in
+  let sparse = Sparse_file.create ~path:fn in
+  let extents = Int_mmap.open_ ~fn:extents_fn ~sz:(File.size extents_fn / 8) in
+  let arr = extents.arr in
+  let arr_sz = BA1.dim arr in
+  let _write_extents_to_sparse = 
+    0 |> iter_k (fun ~k i -> 
+        match i < arr_sz with
+        | false -> ()
+        | true -> 
+          let off,len = arr.{i},arr.{i+1} in
+          Sparse_file.append_region sparse ~src ~src_off:off ~len ~virt_off:off;
+          k (i+2))
+  in
+  let _ = Int_mmap.close extents in
+  let _ = Sparse_file.close sparse in
+  ()
+
+
   
-let create_sparse_file ~(extents_fn:string) ~(pread:Pread.t) ~max_pos = ()
+
