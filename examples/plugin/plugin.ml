bb799e801fed772ef3e99b1e2336bc7bcd113eb4
zach <zachshipko@gmail.com>
2022-04-11 17:20:46 -0700

    Add plugin example

---
+++ examples/plugin/plugin.ml
@@ -0,0 +1,28 @@
+open Irmin_unix
+
+(* Adding a new content type *)
+
+module Int = struct
+  type t = int
+
+  let t = Irmin.Type.int
+  let merge = Irmin.Merge.(option (idempotent t))
+end
+
+let () = Resolver.Contents.add "int" (module Int)
+
+module Schema = struct
+  module Contents = Int
+  module Hash = Irmin.Hash.BLAKE2B
+  module Branch = Irmin.Branch.String
+  module Path = Irmin.Path.String_list
+  module Info = Irmin.Info.Default
+  module Metadata = Irmin.Metadata.None
+end
+
+(* Adding a new store type *)
+
+module Store = Irmin_mem.Make (Schema)
+
+let store = Resolver.Store.v Irmin_mem.Conf.spec (module Store)
+let () = Resolver.Store.add "mem-int" (Fixed store)
