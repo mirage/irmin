ae2e176dd55ad3ac1c89d59bf9399269e5d1bcce
ngoguey42 <ngoguey42@free.fr>
2022-06-30 11:52:21 +0200

    Open polyvars in functions of Io_errors.S

--- src/irmin-pack/unix/io_errors.ml
+++ src/irmin-pack/unix/io_errors.ml
@@ -21,12 +21,20 @@ open Errors
     {!Io.S.misc_error} *)
 module type S = sig
   module Io : Io.S
-  include Errors.S with type t = [ Base.t | `Io_misc of Io.misc_error ]
+
+  type t = [ Base.t | `Io_misc of Io.misc_error ]
+
+  val pp : Format.formatter -> [< t ] -> unit
+  val raise_error : [< t ] -> 'a
+  val log_error : string -> [< t ] -> unit
+  val catch : (unit -> 'a) -> ('a, [> t ]) result
+  val raise_if_error : ('a, [< t ]) result -> 'a
+  val log_if_error : string -> (unit, [< t ]) result -> unit
+  val to_json_string : (int63, [< t ]) result -> string
+  val of_json_string : string -> (int63, [> t ]) result
 end
 
-module Make (Io : Io.S) :
-  S with module Io = Io and type t = [ Base.t | `Io_misc of Io.misc_error ] =
-struct
+module Make (Io : Io.S) : S with module Io = Io = struct
   module Io = Io
 
   type misc_error = Io.misc_error [@@deriving irmin ~pp]
@@ -44,9 +52,7 @@ struct
   let log_error context e = [%log.err "%s failed: %a" context pp e]
 
   let catch f =
-    try Io.catch_misc_error f
-    with _ as ex ->
-      (Base.catch (fun () -> raise ex) : ('a, Base.t) result :> ('a, t) result)
+    try Io.catch_misc_error f with _ as ex -> Base.catch (fun () -> raise ex)
 
   let raise_if_error = function Ok x -> x | Error e -> raise_error e
 
@@ -66,9 +72,6 @@ struct
 
   let of_json_string string =
     match Irmin.Type.of_json_string io_err_result string with
-    | Error (`Msg _) ->
-        (Base.of_json_string string
-          : (int63, Base.t) result
-          :> (int63, t) result)
-    | Ok result -> (result : (int63, io_error) result :> (int63, t) result)
+    | Error (`Msg _) -> Base.of_json_string string
+    | Ok result -> (result : (_, io_error) result :> (_, [> t ]) result)
 end
