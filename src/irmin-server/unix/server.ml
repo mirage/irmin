1ca919a9edecfea455d5c7902b854ffe53170bcf
zach <zachshipko@gmail.com>
2022-08-09 14:09:36 -0700

    Add basic web dashboard

--- src/irmin-server/unix/server.ml
+++ src/irmin-server/unix/server.ml
@@ -254,6 +254,239 @@ module Make (Codec : Conn.Codec.S) (Store : Irmin.Generic_key.S) = struct
 
   let on_exn x = Logs.err (fun l -> l "EXCEPTION: %s" (Printexc.to_string x))
 
+  let dashboard t =
+    let list store prefix =
+      let* keys = Store.list store prefix in
+      let+ keys =
+        Lwt_list.map_s
+          (fun (path, tree) ->
+            let path = Store.Path.rcons prefix path in
+            let* kind = Store.Tree.kind tree Store.Path.empty in
+            match kind with
+            | Some `Contents ->
+                Lwt.return_some (path, "contents", Store.Tree.hash tree)
+            | Some `Node -> Lwt.return_some (path, "node", Store.Tree.hash tree)
+            | None -> Lwt.return_none)
+          keys
+      in
+      List.filter_map Fun.id keys
+    in
+    let data_callback prefix =
+      let* store = Store.main t.repo in
+      let* is_contents =
+        Store.kind store prefix >|= fun x -> x = Some `Contents
+      in
+      let res = Cohttp_lwt_unix.Response.make () in
+      if is_contents then
+        let* contents = Store.get store prefix in
+        let contents' = Irmin.Type.to_json_string Store.contents_t contents in
+        let body =
+          Cohttp_lwt.Body.of_string
+            (Printf.sprintf {|{"contents": %s, "hash": %s}|} contents'
+               (Irmin.Type.to_json_string Store.hash_t
+                  (Store.Contents.hash contents)))
+        in
+        Lwt.return (res, body)
+      else
+        let* keys = list store prefix in
+        let* keys =
+          Lwt_list.map_s
+            (fun (path, kind, hash) ->
+              Format.sprintf {|{"path": "%s", "kind": "%s", "hash": "%s"}|}
+                (Irmin.Type.to_string Store.path_t path)
+                kind
+                (Irmin.Type.to_string Store.hash_t hash)
+              |> Lwt.return)
+            keys
+        in
+        let keys = String.concat "," keys in
+        let body = Cohttp_lwt.Body.of_string (Printf.sprintf "[%s]" keys) in
+        Lwt.return (res, body)
+    in
+    let callback _conn req body =
+      let* () = Cohttp_lwt.Body.drain_body body in
+      let uri = Cohttp_lwt_unix.Request.uri req in
+      let path = Uri.path uri in
+      let prefix = Irmin.Type.of_string Store.path_t path |> Result.get_ok in
+      let meth = Cohttp_lwt_unix.Request.meth req in
+      match meth with
+      | `POST -> data_callback prefix
+      | `GET ->
+          let res = Cohttp_lwt_unix.Response.make () in
+          let body =
+            Cohttp_lwt.Body.of_string
+            @@ Printf.sprintf
+                 {|
+          <html>
+            <head>
+              <meta charset="utf-8"/>
+              <title>Irmin</title>
+              <style>
+                body {
+                  font-family: sans-serif;
+                }
+
+                #list { margin: auto; padding: 5px; }
+                #list li {
+                  list-style-type: none;
+                  margin: 5px;
+                  padding: 0px;
+                  border: 1px solid #ddd;
+                  display: block;
+                }
+
+                #list li:hover {
+                  background: #FFFFCC;
+                  cursor: pointer;
+                }
+
+                #list li span {
+                  display: inline-block;
+                  padding: 10px;
+                }
+
+                #list li span.item-path {
+                  margin-left: 0px;
+                }
+
+                #list li span.item-hash {
+                  margin-left: 10px;
+                  font-family: monospace;
+                }
+
+                #list li span.item-kind {
+                  padding: 2px;
+                  border: 1px solid #ddd;
+                  width: 75px;
+                  text-align: center;
+                }
+
+                #path {
+                  padding: 5px;
+                  margin: 5px;
+                  font-size: 110%%;
+                  border: 1px solid black;
+                }
+
+                #contents {
+                  padding: 5px;
+                  margin: 5px;
+                  font-family: monospace;
+                }
+
+                h1, h2 {
+                  padding: 5px;
+                  margin: 5px;
+                }
+
+                #up:hover {
+                  cursor: pointer;
+                  color: white;
+                  background: black;
+                }
+
+              .no-select {
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+              }
+
+              .text-select {
+                -webkit-user-select: text;
+                -moz-user-select: text;
+                -ms-user-select: text;
+                user-select: text;
+              }
+
+              .content-hash {
+                font-size: 115%%;
+                border: 1px solid #ddd;
+                padding: 10px;
+                margin-bottom: 20px;
+                display: table;
+              }
+              </style>
+            </head>
+            <body class="no-select">
+              <h1 class="no-select">Irmin Explorer</h1>
+              <h2 class="no-select">Path: <input id="path" value="%s"/><span class="no-select" id="up">&uarr;</span></h2>
+              <ul id="list" class="text-select"></ul>
+              <div id="contents" class="text-select"></div>
+              <script>
+                let list = document.getElementById("list");
+                let pathInput = document.getElementById("path");
+                function makeElem(data){
+                  var elem = document.createElement("li");
+                  var path = document.createElement("span");
+                  var hash = document.createElement("span");
+                  var kind = document.createElement("span");
+
+                  path.classList.add("item-path");
+                  path.innerHTML = data.path;
+
+                  kind.classList.add("item-kind");
+                  kind.innerHTML = data.kind;
+
+                  hash.classList.add("item-hash");
+                  hash.innerHTML = data.hash;
+
+                  elem.appendChild(path);
+                  elem.appendChild(kind);
+                  elem.appendChild(hash);
+
+                  elem.onclick = function(){
+                    update(this.firstChild.innerHTML);
+                  }
+                  return elem;
+                }
+
+                function update(path='') {
+                  pathInput.value = path;
+                  fetch(path, {method: 'POST'})
+                  .then(r => r.json())
+                  .then(data => {
+                    list.innerHTML = "";
+                    document.getElementById("contents").innerHTML = "";
+                    if (data.contents !== undefined){
+                      document.getElementById("contents").innerHTML = "<div class='content-hash'>" + data.hash + "</div>" + data.contents;
+                      return;
+                    }
+                    for(var i = 0; i < data.length; i++){
+                      list.appendChild(makeElem(data[i]));
+                    }
+                    history.pushState({path: pathInput.value || "/"}, '');
+                  });
+                }
+
+                update();
+
+                pathInput.onkeydown = function(e){
+                  if (e.keyCode == 13){
+                    console.log(e);
+                    update(e.target.value);
+                  }
+                }
+
+                document.getElementById("up").onclick = function() {
+                  update(pathInput.value.substring(0, pathInput.value.lastIndexOf('/')));
+                }
+
+                window.onpopstate = function(event){
+                  update(event.state.path);
+                }
+              </script>
+            </body>
+          </html>
+          |}
+                 path
+          in
+          Lwt.return (res, body)
+      | _ -> failwith "XXX"
+    in
+    let server = Cohttp_lwt_unix.Server.make ~callback () in
+    Cohttp_lwt_unix.Server.create ~mode:(`TCP (`Port 9999)) server
+
   let serve ?stop t =
     let unlink () =
       match Uri.scheme t.uri with
@@ -270,6 +503,7 @@ module Make (Codec : Conn.Codec.S) (Store : Irmin.Generic_key.S) = struct
           unlink ();
           exit 0)
     in
+    let () = Lwt.async (fun () -> dashboard t) in
     let* () =
       match Uri.scheme t.uri with
       | Some "ws" | Some "wss" ->
