55655c9a4c021657025321b3e93b6c2222d87b01
zach <zachshipko@gmail.com>
2022-08-26 08:05:40 -0700

    Improve constraints for GraphQL IO module

--- src/irmin-graphql/unix/irmin_graphql_unix.mli
+++ src/irmin-graphql/unix/irmin_graphql_unix.mli
@@ -31,6 +31,7 @@ module Server : sig
     Irmin_graphql.Server.S
       with type repo = S.repo
        and type server = Cohttp_lwt_unix.Server.t
+       and module IO = Cohttp_lwt_unix.IO
 
   module Make_ext
       (S : Irmin.Generic_key.S) (Remote : sig
@@ -48,4 +49,5 @@ module Server : sig
     Irmin_graphql.Server.S
       with type repo = S.repo
        and type server = Cohttp_lwt_unix.Server.t
+       and module IO = Cohttp_lwt_unix.IO
 end
