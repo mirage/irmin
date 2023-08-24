1b959e930d204d43dc8eb4f00dd5ffbcb1ddf342
ArthurW <arthur@tarides.com>
2023-08-24 14:16:50 +0200

    eio: irmin-server using lwt_eio

--- test/irmin-client/test.ml
+++ test/irmin-client/test.ml
@@ -14,7 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-open Lwt.Syntax
 open Irmin_client_unix
 open Util
 module Info = Info (Client.Info)
@@ -36,37 +35,17 @@ end
 module Make (R : R) = struct
   let () = at_exit (fun () -> try Unix.kill R.pid Sys.sigint with _ -> ())
   let config = Irmin_client_unix.config R.uri
-  let client = Lwt_main.run (Client.Repo.v config)
-  let clean ~config:_ = Client.Branch.remove client "main"
 
   module X = Irmin_mem.KV.Make (Irmin.Contents.String)
   module Store = Irmin_client_unix.Make (X)
 
-  let suite =
+  let suite () =
+    let client = Client.Repo.v config in
+    let clean ~config:_ = Client.Branch.remove client "main" in
     Irmin_test.Suite.create_generic_key ~name:R.kind
       ~store:(module Store)
       ~config ~clean ()
 end
-
-let kind, pid, uri = run_server `Unix_domain
-
-module Unix_socket = Make (struct
-  let pid = pid
-  let uri = uri
-  let kind = kind
-end)
-
-module Tcp_socket = Make (struct
-  let kind, pid, uri = run_server `Tcp
-end)
-
-module Websocket = Make (struct
-  let kind, pid, uri = run_server `Websocket
-end)
-
-let config = Irmin_client_unix.config uri
-let client = Lwt_main.run (Client.Repo.v config)
-let client () = Client.dup client
 
 let error =
   Alcotest.testable (Fmt.using Error.to_string Fmt.string) (fun a b ->
@@ -77,32 +56,51 @@ let ty t =
     (Fmt.using (Irmin.Type.to_string t) Fmt.string)
     (fun a b -> Irmin.Type.(unstage (equal t)) a b)
 
-let ping () =
+let ping client () =
   let open Client in
-  let* client = client () in
+  let client = client () in
   Logs.debug (fun l -> l "BEFORE PING");
-  let+ r = ping client in
+  let r = ping client in
   Logs.debug (fun l -> l "AFTER PING");
   Alcotest.(check (result unit error)) "ping" (Ok ()) r
 
-let misc = [ ("ping", `Quick, ping) ]
-let misc = [ ("misc", misc) ]
+let misc client = [ ("ping", `Quick, ping client) ]
+let misc client = [ ("misc", misc client) ]
 
-let () =
+let main () =
+  let kind, pid, uri = run_server `Unix_domain in
+  let config = Irmin_client_unix.config uri in
+  let client = Client.Repo.v config in
+  let client () = Lwt_eio.run_lwt @@ fun () -> Client.dup client in
+  let module Unix_socket = Make (struct
+    let pid = pid
+    let uri = uri
+    let kind = kind
+  end) in
+  let module Tcp_socket = Make (struct
+    let kind, pid, uri = run_server `Tcp
+  end) in
+  let module Websocket = Make (struct
+    let kind, pid, uri = run_server `Websocket
+  end) in
   let slow = Sys.getenv_opt "SLOW" |> Option.is_some in
   let only = Sys.getenv_opt "ONLY" in
   let tests =
     match only with
-    | Some "ws" -> [ (`Quick, Websocket.suite) ]
-    | Some "tcp" -> [ (`Quick, Tcp_socket.suite) ]
-    | Some "unix" -> [ (`Quick, Unix_socket.suite) ]
+    | Some "ws" -> [ (`Quick, Websocket.suite ()) ]
+    | Some "tcp" -> [ (`Quick, Tcp_socket.suite ()) ]
+    | Some "unix" -> [ (`Quick, Unix_socket.suite ()) ]
     | Some s -> failwith ("Invalid selection: " ^ s)
     | None ->
         [
-          (`Quick, Unix_socket.suite);
-          (`Quick, Tcp_socket.suite);
-          (`Quick, Websocket.suite);
+          (`Quick, Unix_socket.suite ());
+          (`Quick, Tcp_socket.suite ());
+          (`Quick, Websocket.suite ());
         ]
   in
-  Lwt_main.run
-    (Irmin_test.Store.run "irmin-server" ~sleep:Lwt_unix.sleep ~slow ~misc tests)
+  Irmin_test.Store.run "irmin-server" ~sleep:Eio_unix.sleep ~slow
+    ~misc:(misc client) tests
+
+let () =
+  Eio_main.run @@ fun env ->
+  Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ -> main ()
