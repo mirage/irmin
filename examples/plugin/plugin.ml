f7b52c35d89fbc0130d3977460e4a1f5229024a9
gwenaelle <charles-edouard.lecat@epitech.eu>
2024-01-31 14:40:47 +0100

    clean plugin

--- examples/plugin/plugin.ml
+++ examples/plugin/plugin.ml
@@ -25,11 +25,6 @@ module Int = struct
   let merge = Irmin.Merge.(option (idempotent t))
 end
 
-let () =
-  Eio_main.run @@ fun env ->
-  Irmin_pack_unix.Io.set_env (Eio.Stdenv.fs env);
-  Resolver.Contents.add ~default:true "int" (module Int)
-
 module Schema = struct
   module Contents = Int
   module Hash = Irmin.Hash.BLAKE2B
@@ -48,4 +43,5 @@ let store = Resolver.Store.v Irmin_mem.Conf.spec (module Store)
 let () =
   Eio_main.run @@ fun env ->
   Irmin_pack_unix.Io.set_env (Eio.Stdenv.fs env);
+  Resolver.Contents.add ~default:true "int" (module Int);
   Resolver.Store.add ~default:true "mem-int" (Fixed store)
