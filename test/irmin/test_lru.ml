1fec6fd935e7fd1f8620a7f847aaa4e5bac5c7c4
zach <zachshipko@gmail.com>
2025-12-23 16:46:42 -0800

    fix qcheck lower bounds

--- test/irmin/test_lru.ml
+++ test/irmin/test_lru.ml
@@ -44,8 +44,8 @@ type action = Add of key * int | Clear
 let add k v = Add (k, v)
 let clear = Clear
 
-let gen_action =
-  QCheck.Gen.(oneof_weighted [ (5, map2 add nat_small nat); (1, pure clear) ])
+let[@alert "-deprecated"] gen_action =
+  QCheck.Gen.(frequency [ (5, map2 add int_small nat); (1, pure clear) ])
 
 let print_action = function
   | Add (k, v) -> Fmt.str "add %d %d" k v
